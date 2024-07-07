9b27b2cc2701849dc6d80f8562ea3f489c815ce5
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-07-07 12:14:40 +0530

    Time: 20 ms (77.49%), Space: 22.7 MB (67.64%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,21 +4,42 @@ public:
         vector<vector<int>> ans;
         int n = intervals.size();
         sort(intervals.begin(),intervals.end());
+        //Approach-1
+        // for(int i=0;i<n;i++){
+        //     int start = intervals[i][0];
+        //     int end = intervals[i][1];
+        //     if(!ans.empty() && end<=ans.back()[1]){
+        //         continue;
+        //     }
+        //     for(int j=i+1;j<n;j++){
+        //         if(intervals[j][0]<=end){
+        //             end = max(end,intervals[j][1]);
+        //         }
+        //         else{
+        //             break;
+        //         }
+        //     }
+        //     ans.push_back({start,end});
+        // }
+        // return ans;
+
+        //Approach-2
         for(int i=0;i<n;i++){
             int start = intervals[i][0];
             int end = intervals[i][1];
-            if(!ans.empty() && end<=ans.back()[1]){
-                continue;
+            if(ans.empty()){
+                ans.push_back({start,end});
             }
-            for(int j=i+1;j<n;j++){
-                if(intervals[j][0]<=end){
-                    end = max(end,intervals[j][1]);
+            else{
+                if(intervals[i][0]<=ans.back()[1]){
+                    end = max(end,ans.back()[1]);
                 }
                 else{
-                    break;
+                    ans.push_back({start,end});
+                    continue;
                 }
             }
-            ans.push_back({start,end});
+            ans.back()[1]=end;
         }
         return ans;
     }
