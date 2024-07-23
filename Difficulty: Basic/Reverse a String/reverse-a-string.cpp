aeb7c3783a0892d6a41fcd5b6c844390a84042c3
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-07-23 20:34:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse a String/reverse-a-string.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+        std::string reversed_s = str;
+    std::reverse(reversed_s.begin(), reversed_s.end());
+    return reversed_s;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+    	string s;
+    	cin >> s;
+    	Solution ob;
+    	cout << ob.reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+
+// } Driver Code Ends
