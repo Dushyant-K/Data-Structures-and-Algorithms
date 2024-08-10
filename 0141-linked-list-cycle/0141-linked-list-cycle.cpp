0f5c4867dcb8367d0f14683b804134b450f8a4c0
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-08-10 13:35:04 +0530

    Time: 11 ms (44.61%), Space: 10.7 MB (50.64%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -10,14 +10,24 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         //Approach-1
-        map<ListNode*,int> mpp;
-        ListNode* mover = head;
-        int count=0;
-        while(mover!=nullptr){
-            if(mpp.contains(mover)==true)return true;
-            mpp[mover] = count;
-            mover = mover->next;
-            count++;
+        // map<ListNode*,int> mpp;
+        // ListNode* mover = head;
+        // int count=0;
+        // while(mover!=nullptr){
+        //     if(mpp.contains(mover)==true)return true;
+        //     mpp[mover] = count;
+        //     mover = mover->next;
+        //     count++;
+        // }
+        // return false;
+
+        //Approach-2
+        ListNode* tortoise = head;
+        ListNode* hare = head;
+        while(hare!=nullptr && hare->next!=nullptr){
+            tortoise = tortoise->next;
+            hare = hare->next->next; 
+             if(hare==tortoise)return true;
         }
         return false;
     }
