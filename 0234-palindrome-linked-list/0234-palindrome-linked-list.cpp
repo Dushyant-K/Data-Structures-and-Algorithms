796bcdce2bbced3114e473262fa70b012a1654d9
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-08-10 17:26:37 +0530

    Time: 168 ms (60.1%), Space: 116.9 MB (66.82%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,20 +10,48 @@
  */
 class Solution {
 public:
+    ListNode* reverseLL(ListNode* head){
+        ListNode* temp = head;
+        ListNode* prev = nullptr;
+        while(temp!=nullptr){
+            head = head->next;
+            temp->next = prev;
+            prev = temp;
+            temp = head;
+        }
+        return prev;
+    }
     bool isPalindrome(ListNode* head) {
         //Approach-1
-        stack<int> st;
-        ListNode* mover = head;
-        while(mover!=nullptr){
-            st.push(mover->val);
-            mover = mover->next;
-        }
+        // stack<int> st;
+        // ListNode* mover = head;
+        // while(mover!=nullptr){
+        //     st.push(mover->val);
+        //     mover = mover->next;
+        // }
 
-        mover = head;
-        while(mover!=nullptr){
-            if(mover->val!=st.top())return false;
-            st.pop();
-            mover = mover->next;
+        // mover = head;
+        // while(mover!=nullptr){
+        //     if(mover->val!=st.top())return false;
+        //     st.pop();
+        //     mover = mover->next;
+        // }
+        // return true;
+
+        //Approach-2
+        ListNode* tortoise = head;
+        ListNode* hare = head;
+        while(hare->next!=nullptr && hare->next->next!=nullptr){
+            tortoise = tortoise->next;
+            hare = hare->next->next;
+        }
+        ListNode* mid = tortoise->next;
+        ListNode* first = head;
+        ListNode* second = reverseLL(mid);
+        while(second!=nullptr){
+            if(first->val!=second->val)return false;
+            first = first->next;
+            second = second->next;
         }
         return true;
     }
