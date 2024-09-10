a39839107e720e64a6a2e968145ba4bbdbf60662
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-09-10 19:21:15 +0530

    Time: 7 ms (81.06%), Space: 10.9 MB (84.83%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,19 +2,40 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
     //Approach-1
+    // int n = s.length();
+    // if(n==0)return 0;
+    // int maxLength=0;
+    // for(int i=0;i<n;i++){
+    //     unordered_set<int> st;
+    //     for(int j=i;j<n;j++){
+    //        if(st.find(s[j])!=st.end()){
+    //         break;
+    //        }
+    //        int length = (j-i+1);
+    //         maxLength = max(length,maxLength);
+    //        st.insert(s[j]);
+    //     }
+    // }
+    // return maxLength;
+
+    //Approach-
+    
     int n = s.length();
-    if(n==0)return 0;
-    int maxLength=0;
-    for(int i=0;i<n;i++){
-        unordered_set<int> st;
-        for(int j=i;j<n;j++){
-           if(st.find(s[j])!=st.end()){
-            break;
-           }
-           int length = (j-i+1);
-            maxLength = max(length,maxLength);
-           st.insert(s[j]);
+    int l =0;
+    int r =0;
+    int maxLength = 0;
+    int hash[256];
+    fill(begin(hash), end(hash), -1);
+    while(r<n){
+        if(hash[s[r]]!=-1){
+            if(hash[s[r]]>=l){
+                l = hash[s[r]]+1;
+            }
         }
+        int length = (r-l+1);
+        maxLength = max(length,maxLength);
+        hash[s[r]] = r;
+        r++;
     }
     return maxLength;
     }
