185c135353fec4bcf3e0a11a9fd5782e80e98909
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-09-13 22:42:53 +0530

    Time: 0 ms (100%), Space: 10.1 MB (72.33%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -24,22 +24,49 @@ public:
         // return ans;
 
         //Approach-2
-        vector<int> ans;
-        if(root==nullptr)return ans;
-        stack<TreeNode*> st1;
-        stack<TreeNode*> st2;
-        st1.push(root);
-        while(!st1.empty()){
-            TreeNode* node = st1.top();
-            st1.pop();
-            if(node->left!=nullptr)st1.push(node->left);
-            if(node->right!=nullptr)st1.push(node->right);
-            st2.push(node);
+        // vector<int> ans;
+        // if(root==nullptr)return ans;
+        // stack<TreeNode*> st1;
+        // stack<TreeNode*> st2;
+        // st1.push(root);
+        // while(!st1.empty()){
+        //     TreeNode* node = st1.top();
+        //     st1.pop();
+        //     if(node->left!=nullptr)st1.push(node->left);
+        //     if(node->right!=nullptr)st1.push(node->right);
+        //     st2.push(node);
+        // }
+        // while(!st2.empty()){
+        //     ans.push_back(st2.top()->val);
+        //     st2.pop();
+        // }
+        // return ans;
+
+        //Approach-3
+        vector<int> result;
+    if (root == nullptr) return result; // If the tree is empty
+
+    stack<TreeNode*> s;
+    TreeNode* current = root;
+    TreeNode* lastVisited = nullptr;
+
+    while (!s.empty() || current != nullptr) {
+        if (current != nullptr) {
+            s.push(current);
+            current = current->left;  // Traverse to the left subtree
+        } else {
+            TreeNode* peekNode = s.top();
+            // If right child exists and traversing node from left child, move to right child
+            if (peekNode->right != nullptr && lastVisited != peekNode->right) {
+                current = peekNode->right;
+            } else {
+                result.push_back(peekNode->val);
+                lastVisited = s.top();
+                s.pop();
+            }
         }
-        while(!st2.empty()){
-            ans.push_back(st2.top()->val);
-            st2.pop();
-        }
-        return ans;
+    }
+
+    return result;
     }
 };
