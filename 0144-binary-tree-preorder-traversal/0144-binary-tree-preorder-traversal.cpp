f3ac68832cfacd59e241caa41c6a5d7a6a24fc0b
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-09-16 23:53:21 +0530

    Time: 0 ms (100%), Space: 10.1 MB (77.42%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -20,8 +20,51 @@ public:
     }
    
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        recursion(root,ans);
-        return ans;
+        //Recursive Approach
+        // vector<int> ans;
+        // recursion(root,ans);
+        // return ans;
+
+        //Approach-2
+        // vector<int> ans;
+        // if(root==nullptr)return ans;
+        // stack<TreeNode*> st;
+        // st.push(root);
+        // while(!st.empty()){
+        //     TreeNode* temp = st.top();
+        //     st.pop();
+        //     if(temp->right!=nullptr)st.push(temp->right);
+        //     if(temp->left!=nullptr)st.push(temp->left);
+        //     ans.push_back(temp->val);
+        // }
+        // return ans;
+
+        //Approach-3(Morris pre-order Traversal)
+        vector<int> preorder;
+        if(root==nullptr)return preorder;
+        TreeNode* curr = root;
+        while(curr!=nullptr){
+            if(curr->left==nullptr){
+                preorder.push_back(curr->val);
+                curr = curr->right;
+            }
+            else{
+                TreeNode* prev = curr->left;
+                while(prev->right&&prev->right!=curr){
+                    prev = prev->right;
+                }
+                if(prev->right==nullptr){
+                    prev->right=curr;
+                    preorder.push_back(curr->val);
+                    curr = curr->left;
+                }
+                else{
+                    prev->right = nullptr;
+                    curr = curr->right;
+                }
+            }
+        }
+        return preorder;
     }
+
 };
