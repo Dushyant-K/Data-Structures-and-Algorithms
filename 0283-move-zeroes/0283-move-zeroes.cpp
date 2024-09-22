af38198e51a525c220ae3089b4cec81d7cce1ad3
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-09-22 12:10:36 +0530

    Time: 7 ms (98.66%), Space: 21.9 MB (74.79%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -11,13 +11,24 @@ public:
         if(j==-1){
             return;
         }
-        for(int i=j+1;i<nums.size();i++){
+        // for(int i=j+1;i<nums.size();i++){
+        //     if(nums[i]!=0){
+        //         int temp=nums[i];
+        //         nums[i]=nums[j];
+        //         nums[j]=temp;
+        //         j++;
+        //     }
+        // }
+
+        int i=j+1;
+        while(i<nums.size()){
             if(nums[i]!=0){
-                int temp=nums[i];
+                int temp = nums[i];
                 nums[i]=nums[j];
                 nums[j]=temp;
                 j++;
             }
+            i++;
         }
     }
 };
