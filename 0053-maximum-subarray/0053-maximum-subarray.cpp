f853684b2f6f2816f4d37c701258fb69ba7c619e
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-09-25 18:09:09 +0530

    Time: 80 ms (40.08%), Space: 70.5 MB (89.53%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-     int maxSum=nums[0];
-     int sum=0;
-     int start=0;
-     int end=0;
-     for(int i=0;i<nums.size();i++){
-        sum+=nums[i];
-        if(sum>maxSum){
-            maxSum=sum;
-            // end=i;
+    //Approach-1(Kadane's Algorithm)
+    int n = nums.size();
+    int sum=0;
+    int maxSum=INT_MIN;
+    int left=0;
+    int right=0;
+    while(right<n){
+        sum+=nums[right];
+        maxSum = max(maxSum,sum);
+        while(sum<0&&left<=right){
+            sum-=nums[left];
+            left++;
         }
-        if(sum<0){
-            sum=0;
-            // start=i;
-        }
-     }   
+        right++;
+    }
+    //Approach-1
+    //  int maxSum=nums[0];
+    //  int sum=0;
+    //  int start=0;
+    //  int end=0;
+    //  for(int i=0;i<nums.size();i++){
+    //     sum+=nums[i];
+    //     if(sum>maxSum){
+    //         maxSum=sum;
+    //         // end=i;
+    //     }
+    //     if(sum<0){
+    //         sum=0;
+    //         // start=i;
+    //     }
+    //  }   
      return maxSum;
     }
 };
