863c8a7683aa4a64882c166bc58304ef16d52b84
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-10-01 11:13:10 +0530

    Time: 3 ms (69.67%), Space: 11.1 MB (91.88%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -4,11 +4,9 @@ public:
         if (strs.empty()) {
         return "";
     }
-    
-    // Assume the first string is the common prefix
+
     string prefix = strs[0];
     
-    // Compare the prefix with each string in the array
     for (size_t i = 1; i < strs.size(); ++i) {
         while (strs[i].find(prefix) != 0) {
             // Shorten the prefix
