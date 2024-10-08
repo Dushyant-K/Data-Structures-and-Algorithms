5a822140e1c0815e37dd7be6b386b76f4bd70088
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-10-08 07:45:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bit Manipulation/bit-manipulation.cpp
+++ Difficulty: Easy/Bit Manipulation/bit-manipulation.cpp
@@ -6,19 +6,29 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
+    int getBit(int num, int i){
+        return (num & (1<<i));
+    }
+    void setBit(int num, int i){
+        (num | (1<<i));
+    }
+    void clearBit(int num, int i){
+        int helper = ~(1<<i);
+         (num & helper);
+    }
     void bitManipulation(int num, int i) {
-        //Get ith bit
-        if(num & (1<<(i-1))){
-        cout<<"1 ";
-        }
-        else
-        cout<<"0 ";
-        
-        //Set ith bit
-        cout<<(num | (1<<(i-1)))<<" ";
-        
-        //Clear ith bit
-        cout<<(num & ~(1<<(i-1)));
+        // Approach-1
+       int get = (num & (1<<(i-1)));
+       if(get!=0){
+           cout<<"1 ";
+       }
+       else cout<<"0 ";
+       
+       int set = (num | (1<<(i-1)));
+       cout<<set<<" ";
+       
+       int clear = (num & ~(1<<(i-1)));
+       cout<<clear;
     }
 };
 
