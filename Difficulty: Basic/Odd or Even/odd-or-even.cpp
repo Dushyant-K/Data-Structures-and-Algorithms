a0f8f5755eb6023dd14a84c80dc5a65c28040ed5
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-10-08 08:13:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Odd or Even/odd-or-even.cpp
+++ Difficulty: Basic/Odd or Even/odd-or-even.cpp
@@ -7,11 +7,9 @@ using namespace std;
 class Solution {
   public:
     string oddEven(int n) {
-         if (n & 1) {
-        return "odd";
-    } else {
+        // Approach-1
+        if(n & 1)return "odd";
         return "even";
-    }
     }
 };
 
