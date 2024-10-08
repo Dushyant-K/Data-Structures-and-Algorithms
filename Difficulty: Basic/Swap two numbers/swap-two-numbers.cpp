30a8ae2ca4e93c0a057afb75c9f34e3c6888c3b9
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-10-08 08:28:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Swap two numbers/swap-two-numbers.cpp
+++ Difficulty: Basic/Swap two numbers/swap-two-numbers.cpp
@@ -10,10 +10,12 @@ using namespace std;
 class Solution{   
 public:
     pair<int, int> get(int a, int b){
-        a = a ^ b;
-        b = a ^ b;
-        a = a ^ b;
-        return make_pair(a,b);
+        //Approacch-1
+         a = a^b;
+         b = a^b;
+         a = a^b;
+         
+         return {a,b};
     }
 };
 
