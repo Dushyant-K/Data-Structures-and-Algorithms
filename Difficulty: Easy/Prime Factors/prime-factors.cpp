8074988d01716f8ba19bb9c9cdd49eafcf73652a
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-10-08 17:22:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Prime Factors/prime-factors.cpp
+++ Difficulty: Easy/Prime Factors/prime-factors.cpp
@@ -9,47 +9,19 @@ using namespace std;
 
 class Solution{
 	public:
-	bool isPrime(int n){
-	    if(n==1 || n==0)return false;
-	    int count=0;
-	    for(int i=1;i<=sqrt(n);i++){
-	        if(n%i==0)count++;
-	        
-	        if((n/i)!=i){
-	           count++;
-	        }
-	        
-	        if(count>2)break;
-	    }
-	    if(count==2)return true;
-	    
-	    return false;
-	}
 	vector<int>AllPrimeFactors(int N) {
-	   // vector<int> ans;
-	   // for(int i=1;i<=sqrt(N);i++){
-	   //     if(N%i==0){
-	   //         if(isPrime(i))ans.push_back(i);
-	            
-	   //         if((N/i)!=i){
-	   //             if(isPrime(N/i))ans.push_back(N/i);
-	   //         }
-	   //     }
-	   // }
-	   // sort(ans.begin(),ans.end());
-	   // return ans;
-	   
-	   //Approach-2
-	   vector<int> ans;
-	   for(int i=2;i<=N;i++){
-	       if(N%i==0){
-	           ans.push_back(i);
-	           while(N%i==0){
-	               N = N/i;
-	           }
-	       }
-	   }
-	   return ans;
+	    // Approach-1
+	    vector<int> ans;
+	    for(int i=2;i<=sqrt(N);i++){
+	        if(N%i==0){
+	            ans.push_back(i);
+	            while(N%i==0){
+	                N= N/i;
+	            }
+	        }
+	    }
+	    if(N!=1)ans.push_back(N);
+	    return ans;
 	}
 };
 
