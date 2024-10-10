752819bacbd3b6a95c7708ce9e94599cc047f769
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-10-10 11:55:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Prefix to Infix Conversion/prefix-to-infix-conversion.cpp
+++ Difficulty: Medium/Prefix to Infix Conversion/prefix-to-infix-conversion.cpp
@@ -10,25 +10,23 @@ using namespace std;
 class Solution {
   public:
     string preToInfix(string pre_exp) {
-        int i = pre_exp.length() - 1;
+        // Approach-1
+        int i=pre_exp.length()-1;
         stack<string> st;
-
-        while (i >= 0) {
-            // If the character is an operand, push it to the stack.
-            if ((pre_exp[i] >= 'A' && pre_exp[i] <= 'Z') || (pre_exp[i] >= 'a' && pre_exp[i] <= 'z') || (pre_exp[i] >= '0' && pre_exp[i] <= '9')) {
-                string s(1, pre_exp[i]); // Convert char to string.
-                st.push(s);
+        while(i>=0){
+            if((pre_exp[i]>='a'&&pre_exp[i]<='z')||(pre_exp[i]>='A'&&pre_exp[i]<='Z')||(pre_exp[i]>='0'&&pre_exp[i]<='9')){
+                st.push(string(1,pre_exp[i]));//1 is the number of times the character is repeated (since you want only 1 occurrence of the character).
+                                         //char_value is the character you want to convert.
             }
-            // If the character is an operator.
-            else {
-                // Pop two operands from the stack.
-                string t1 = st.top(); st.pop();
-                string t2 = st.top(); st.pop();
-                // Create a new string with the operator between the operands.
-                string temp = '(' + t1 + pre_exp[i] + t2 + ')';
-                st.push(temp);
+            else{
+                string top1=st.top();
+                st.pop();
+                string top2=st.top();
+                st.pop();
+                string conv="("+top1+pre_exp[i]+top2+")";
+                st.push(conv);
             }
-            i--; // Move to the previous character.
+            i--;
         }
         return st.top();
     }
