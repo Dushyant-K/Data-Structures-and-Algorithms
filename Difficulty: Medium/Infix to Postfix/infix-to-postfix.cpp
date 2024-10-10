c2348ca26bc59b7a8db6ddac36d1243c5628503c
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-10-10 09:59:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Infix to Postfix/infix-to-postfix.cpp
+++ Difficulty: Medium/Infix to Postfix/infix-to-postfix.cpp
@@ -7,35 +7,35 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int priority(char ch){
-        if(ch=='^')return 3;
-        else if(ch=='*'||ch=='/')return 2;
-        else if(ch=='+'||ch=='-')return 1;
-        else return -1;
-    }
+     int priority(char a){
+         if(a=='^')return 3;
+         else if(a=='*'||a=='/')return 2;
+         else if(a=='+'||a=='-')return 1;
+         return -1;
+     }
     // Function to convert an infix expression to a postfix expression.
     string infixToPostfix(string s) {
-        int i=0;
-        stack<char> st;
+        // Approach-1
+        stack<int> st;
         string ans="";
-        
+        int i=0;
         while(i<s.length()){
             if((s[i]>='A'&&s[i]<='Z')||(s[i]>='a'&&s[i]<='z')||(s[i]>='0'&&s[i]<='9')){
-                ans = ans+s[i];
+                ans+=s[i];
             }
             else if(s[i]=='('){
                 st.push(s[i]);
             }
             else if(s[i]==')'){
-                while(!(st.empty())&&st.top()!='('){
-                    ans = ans+st.top();
+                while(!st.empty()&&st.top()!='('){
+                    ans+=st.top();
                     st.pop();
                 }
                 st.pop();
             }
             else{
-                while(!(st.empty())&&(priority(s[i])<=priority(st.top()))){
-                    ans = ans+st.top();
+                while(!st.empty()&&priority(s[i])<=priority(st.top())){
+                    ans+=st.top();
                     st.pop();
                 }
                 st.push(s[i]);
@@ -43,7 +43,7 @@ class Solution {
             i++;
         }
         while(!st.empty()){
-            ans = ans+st.top();
+            ans+=st.top();
             st.pop();
         }
         return ans;
