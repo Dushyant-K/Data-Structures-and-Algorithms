92f9f94d5be4d3916fd30bfaee1a7bbf52fe6753
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-10-10 12:02:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Postfix to Prefix Conversion/postfix-to-prefix-conversion.cpp
+++ Difficulty: Medium/Postfix to Prefix Conversion/postfix-to-prefix-conversion.cpp
@@ -10,18 +10,20 @@ using namespace std;
 class Solution {
   public:
     string postToPre(string post_exp) {
-        int i=0;
+        // Approach-1
         stack<string> st;
+        int i=0;
         while(i<post_exp.length()){
-            if((post_exp[i]>='A'&&post_exp[i]<='Z')||(post_exp[i]>='a'&&post_exp[i]<='z')||post_exp[i]>='0'&&post_exp[i]=='9'){
-                string s(1,post_exp[i]);
-                st.push(s);
+            if((post_exp[i]>='A'&&post_exp[i]<='Z')||(post_exp[i]>='a'&&post_exp[i]<='z')||(post_exp[i]>='0'&&post_exp[i]<='9')){
+                st.push(string(1,post_exp[i]));
             }
             else{
-                string t1 = st.top();st.pop();
-                string t2 = st.top();st.pop();
-                string temp = post_exp[i]+t2+t1;
-                st.push(temp);
+                string top1=st.top();
+                st.pop();
+                string top2=st.top();
+                st.pop();
+                string conv=post_exp[i]+top2+top1;
+                st.push(conv);
             }
             i++;
         }
