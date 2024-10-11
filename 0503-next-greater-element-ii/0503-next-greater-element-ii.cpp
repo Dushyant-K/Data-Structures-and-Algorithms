2a3c72259b3afc52037771ed5c7d931d9d55fd0e
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-10-11 08:59:55 +0530

    Time: 24 ms (45.49%), Space: 27.8 MB (13.12%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,20 +1,51 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        vector<int> ans;
+        //Approach-1
+        // vector<int> ans;
+        // int n = nums.size();
+        // for(int i=0;i<n;i++){
+        //     ans.push_back(-1);
+        // }
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<=(i+n-1);j++){
+        //         int index = j%n;
+        //         if(nums[i]<nums[index]){
+        //             ans[i]=nums[index];
+        //             break;
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        //Approach-2
         int n = nums.size();
-        for(int i=0;i<n;i++){
-            ans.push_back(-1);
-        }
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<=(i+n-1);j++){
-                int index = j%n;
-                if(nums[i]<nums[index]){
-                    ans[i]=nums[index];
-                    break;
+        stack<int> st;
+        int ans[n];
+        for(int i=2*n-1;i>=0;i--){
+            if(i>=n){
+                while(!(st.empty())&&(nums[i%n]>st.top())){
+                    st.pop();
                 }
+                st.push(nums[i%n]);
+            }
+            else{
+                while(!(st.empty())&&(nums[i]>=st.top())){
+                    st.pop();
+                }
+                if(st.empty()){
+                ans[i]=-1;
+            }
+            else{
+                ans[i]=st.top();
+            }
+             st.push(nums[i]);
             }
         }
-        return ans;
+        vector<int> nge;
+        for(int i=0;i<n;i++){
+            nge.push_back(ans[i]);
+        }
+        return nge;
     }
 };
