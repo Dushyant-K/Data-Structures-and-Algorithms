08581ac86fc45d4cb31af411af6eafba4ac4a89a
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-10-17 00:12:00 +0530

    Time: 67 ms (78.24%), Space: 86.1 MB (53.43%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
+        //Approach-1
+        int countFive=0;
+        int countTen=0;
         int n = bills.size();
-        int five =0;
-        int ten =0;
         for(int i=0;i<n;i++){
-            if(bills[i]==5)five++;
+            if(bills[i]==5){
+                countFive++;
+            }
             else if(bills[i]==10){
-                ten++;
-                if(five>0)five--;
-                else return false;
+                if(countFive==0)return false;
+                countFive--;
+                countTen++;
             }
             else{
-                if(ten&&five){
-                    ten--;
-                    five--;
+                if(countFive>=1&&countTen>=1){
+                    countFive = countFive-1;
+                    countTen = countTen-1;
                 }
-                else if(five>=3)five-=3;
-                else return false;
+                else if(countFive>=3){
+                    countFive-=3;
+                }
+                else{
+                    return false;
+                }
             }
         }
         return true;
