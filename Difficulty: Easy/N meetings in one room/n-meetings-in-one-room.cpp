0958b36d3f73f91fa5894dabf04595711bcdde5e
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-10-17 09:43:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
+++ Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
@@ -6,58 +6,70 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    // Function to find the maximum number of meetings that can
-    // be performed in a meeting room.
-    class meetings{
-    public:
-        int start;
-        int end;
-        int pos;
-    };
-     static bool comparator(meetings meet1, meetings meet2){
-        if (meet1.end < meet2.end) return true;
-        else if (meet1.end > meet2.end) return false;
-        else return meet1.pos < meet2.pos;
-    }
-    int maxMeetings(int n, int start[], int end[]) {
-         meetings arr[n];
-         vector<int> order;
+   //Approach-1
+   class meetings{
+       public:
+       int start;
+       int end;
+       int pos;
+   };
+   static bool comparator(meetings meet1, meetings meet2){
+       if(meet1.end<meet2.end)return true;
+       else if(meet1.end>meet2.end)return false;
+       else return meet1.pos<meet2.pos;
+   }
+    int maxMeetings(vector<int> start, vector<int> end) {
+        // Approach-1
+        int n = start.size();
+        meetings arr[n];
         for(int i=0;i<n;i++){
             arr[i].start = start[i];
-            arr[i].end = end[i];
+            arr[i].end  = end[i];
             arr[i].pos = i+1;
+        }
+        vector<int> order;
+        sort(arr,arr+n, comparator);
+        int freeTime = arr[0].end;
+        order.push_back(arr[0].pos);
+        int count=1;
+        for(int i=1;i<n;i++){
+            if(freeTime<arr[i].start){
+                count++;
+                order.push_back(arr[i].pos);
+                freeTime=arr[i].end;
             }
-            sort(arr,arr+n,comparator);
-            int count=1;
-            order.push_back(arr[0].pos);
-            int freeTime=arr[0].end;
-            for(int i=1;i<n;i++){
-                if(arr[i].start>freeTime){
-                    count++;
-                    freeTime = arr[i].end;
-                    order.push_back(arr[i].pos);
-                }
-            }
-            return count;
+        }
+        return count;
     }
 };
 
 //{ Driver Code Starts.
+
 int main() {
+
     int t;
     cin >> t;
+    cin.ignore();
     while (t--) {
-        int n;
-        cin >> n;
-        int start[n], end[n];
-        for (int i = 0; i < n; i++)
-            cin >> start[i];
+        string input;
+        int num;
+        vector<int> start;
 
-        for (int i = 0; i < n; i++)
-            cin >> end[i];
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            start.push_back(num);
+        }
+
+        vector<int> end;
+        getline(cin, input);
+        stringstream s22(input);
+        while (s22 >> num) {
+            end.push_back(num);
+        }
 
         Solution ob;
-        int ans = ob.maxMeetings(n, start, end);
+        int ans = ob.maxMeetings(start, end);
         cout << ans << endl;
     }
     return 0;
