83a9f3a1b606105165ba078d8feb0c6b9a1151e7
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-10-20 21:20:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.cpp
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.cpp
@@ -6,58 +6,24 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    // Function to find a continuous sub-array which adds up to a given number.
-    vector<int> subarraySum(vector<int> arr, int n, long long s) {
+    vector<int> subarraySum(vector<int> &arr, int s) {
+        // Approach-1
+        int n = arr.size();
         int left=0;
         int right=0;
-        long long sum=0;
-        vector<int> ans;
-        
-        // if(s==0){
-        // for(int i=0 ; i<n ; i++){
-        //     if(arr[i] == 0)
-        //         return {i+1 , i+1};
-        // }
-        // }
-        //  for(int i=0 ; i<n ; i++){
-        //     if(arr[i] == s)
-        //         return {i+1 , i+1};
-        // }
-
-        if(s==0){
-            for(int i=0 ; i<n ; i++)
-            {
-                if(arr[i]==0)
-                    return {i+1 , i+1};
-               
-            }
-            return {-1};
-        }
-            
-        
+        int sum=0;
         while(right<n){
             sum+=arr[right];
-            
-            while(sum>s && left<=right){
+            while(sum>s){
                 sum-=arr[left];
                 left++;
             }
-            
-            // if(left == right+1 && sum == 0)
-            //   return {-1};
-            
-            if(sum==s && s!=0){
-                ans.push_back(left+1);
-                ans.push_back(right+1);
-                return ans;
+            if(sum==s){
+                return {left+1,right+1};
             }
-            
-                right++;
+            right++;
         }
-        
-        ans.push_back(-1);
-        return ans;
-         
+        return {-1};
     }
 };
 
@@ -66,24 +32,30 @@ class Solution {
 int main() {
     int t;
     cin >> t;
+    cin.ignore(); // Ignore the newline character after t
     while (t--) {
-        int n;
-        long long s;
-        cin >> n >> s;
-        vector<int> arr(n);
-        // int arr[n];
-        const int mx = 1e9;
-        for (int i = 0; i < n; i++) {
-            cin >> arr[i];
-        }
-        Solution ob;
-        vector<int> res;
-        res = ob.subarraySum(arr, n, s);
+        vector<int> arr;
+        int d;
+        string input;
 
-        for (int i = 0; i < res.size(); i++)
-            cout << res[i] << " ";
-        cout << endl;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        cin >> d;
+        cin.ignore(); // Ignore the newline character after d
+
+        Solution ob;
+        vector<int> result = ob.subarraySum(arr, d);
+        for (int i : result) {
+            cout << i << " ";
+        }
+        cout << "\n";
     }
     return 0;
 }
+
 // } Driver Code Ends
