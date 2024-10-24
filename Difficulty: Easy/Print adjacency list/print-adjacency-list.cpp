54e4f9095b790102a08f8505164fb3afe0d5647b
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-10-24 15:33:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Print adjacency list/print-adjacency-list.cpp
+++ Difficulty: Easy/Print adjacency list/print-adjacency-list.cpp
@@ -2,32 +2,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 class Solution {
   public:
     // Function to return the adjacency list for each vertex.
-    vector<vector<int>> printGraph(int V, vector<pair<int,int>>edges) {
+    vector<vector<int>> printGraph(int V, vector<pair<int, int>>& edges) {
         // Approach-1
-        // int adjMatrix[V][V];
-        // int m = edges.size();
-        // for(auto it: edges){
-        //     int first = it.first;
-        //     int second = it.second;
-        //     adjMatrix[first][second]=1;
-        //     adjMatrix[second][first]=1;
-        // }
-        
-        //Approach-2
-        // vector<int> adjList[V];
-        // for(auto it: edges){
-        //     int u = it.first;
-        //     int v = it.second;
-        //     adjList[u].push_back(v);
-        //     adjList[v].push_back(u);
-        // }
-        // return adjList;
-        
-        //Approach-3
         vector<vector<int>> adjList(V);
         for(auto it:edges){
             int u = it.first;
@@ -46,22 +27,39 @@ int main() {
     while (tc--) {
         int V, E;
         cin >> V >> E;
-        vector<pair<int,int>>edges;
+        vector<pair<int, int>> edges;
         for (int i = 0; i < E; i++) {
             int u, v;
             cin >> u >> v;
-            edges.push_back({u,v});
+            edges.push_back({u, v});
         }
         Solution obj;
         vector<vector<int>> adj = obj.printGraph(V, edges);
-        for(int i=0;i<V;i++)
-        {
-            sort(adj[i].begin(),adj[i].end());
-            for(auto it:adj[i])
-                cout<<it<<" ";
-            cout<<endl;
+        bool empty = true;
+        for (int i = 0; i < V; i++) {
+            if (adj[i].empty())
+                continue;
+            empty = false;
+            break;
+        }
+        if (empty) {
+            cout << "[]\n";
+            continue;
+        }
+        for (int i = 0; i < V; i++) {
+            set<int> st(adj[i].begin(), adj[i].end());
+            cout << "[";
+            auto it = st.begin();
+
+            while (it != st.end()) {
+                cout << *it;
+                if (next(it) != st.end())
+                    cout << " ";
+                ++it;
+            }
+            cout << "]";
+            cout << endl;
         }
     }
-    return 0;
 }
 // } Driver Code Ends
