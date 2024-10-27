03dd3fde51fdd1c63319e01606e2645b91d5ae12
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-10-27 11:37:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -1,111 +1,78 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
-class Solution
-{
-	public:
-	//Function to find the shortest distance of all the vertices
-    //from the source vertex S.
-    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
-    {
-        // Approach-1(This approach didnt work understand why)
-        vector<int> dist(V,1e9);
-        queue<pair<int,int>> q;
-        dist[S]=0;
-        q.push({0,S});
-        while(!q.empty()){
-            int distance = q.front().first;
-            int node = q.front().second;
-            q.pop();
-            for(auto it: adj[node]){
-                int adjNode = it[0];
-                int weight = it[1];
-                if(distance+weight<dist[adjNode]){
-                    dist[adjNode]=distance+weight;
-                    q.push({dist[adjNode],adjNode});
+// User Function Template
+class Solution {
+  public:
+    // Function to find the shortest distance of all the vertices
+    // from the source vertex src.
+    vector<int> dijkstra(vector<vector<pair<int, int>>> &adj, int src) {
+        // Approach-1(Using a set)
+        int n = adj.size();
+        
+        vector<int> distance(n,1e9);
+        set<pair<int,int>> st;
+        distance[src]=0;
+        st.insert({0,src});
+        
+        //Begin the iteration
+        while(!st.empty()){
+            auto it = *(st.begin());
+            st.erase(it);
+            int dist = it.first;
+            int node = it.second;
+            
+            for(auto i:adj[node]){
+                int neighbour = i.first;
+                int weight = i.second;
+                if(distance[neighbour]>dist+weight){
+                    
+                    if(distance[neighbour]!=1e9){
+                        st.erase({distance[neighbour],neighbour});
+                    }
+                    
+                    distance[neighbour]=dist+weight;
+                    st.insert({distance[neighbour],neighbour});
                 }
             }
         }
-        for(int i=0;i<V;i++){
-            if(dist[i]==1e9)dist[i]=-1;
-        }
-        return dist;
-        
-        //Approach-2
-        //   vector<int> dist(V, 1e9);
-        // dist[S] = 0;
-
-        // // Min-heap priority queue to get the node with the smallest distance
-        // priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        // pq.push({0, S});
-
-        // // Standard Dijkstra's Algorithm
-        // while (!pq.empty()) {
-        //     int nodeDist = pq.top().first;
-        //     int node = pq.top().second;
-        //     pq.pop();
-
-        //     // Traverse through all adjacent nodes of the current node
-        //     for (auto it : adj[node]) {
-        //         int adjNode = it[0];
-        //         int edgeWeight = it[1];
-
-        //         // If a shorter path to adjNode is found
-        //         if (nodeDist + edgeWeight < dist[adjNode]) {
-        //             dist[adjNode] = nodeDist + edgeWeight;
-        //             pq.push({dist[adjNode], adjNode});
-        //         }
-        //     }
-        // }
-
-        // // Replace unreachable nodes' distances with -1
-        // for (int i = 0; i < V; i++) {
-        //     if (dist[i] == 1e9) dist[i] = -1;
-        // }
-
-        // return dist;
+        return distance;
     }
 };
 
 
 //{ Driver Code Starts.
 
-
-int main()
-{
+int main() {
     int t;
     cin >> t;
     while (t--) {
         int V, E;
         cin >> V >> E;
-        vector<vector<int>> adj[V];
-        int i=0;
-        while (i++<E) {
+        vector<vector<pair<int, int>>> adj(V);
+        int i = 0;
+        while (i++ < E) {
             int u, v, w;
             cin >> u >> v >> w;
-            vector<int> t1,t2;
-            t1.push_back(v);
-            t1.push_back(w);
+            pair<int, int> t1 = {v, w}, t2 = {u, w};
             adj[u].push_back(t1);
-            t2.push_back(u);
-            t2.push_back(w);
             adj[v].push_back(t2);
         }
-        int S;
-        cin>>S;
-        
+        int src;
+        cin >> src;
+        cin.ignore();
+
         Solution obj;
-    	vector<int> res = obj.dijkstra(V, adj, S);
-    	
-    	for(int i=0; i<V; i++)
-    	    cout<<res[i]<<" ";
-    	cout<<endl;
+        vector<int> res = obj.dijkstra(adj, src);
+
+        for (int i = 0; i < V; i++)
+            cout << res[i] << " ";
+        cout << endl;
     }
 
     return 0;
 }
-
-
 // } Driver Code Ends
