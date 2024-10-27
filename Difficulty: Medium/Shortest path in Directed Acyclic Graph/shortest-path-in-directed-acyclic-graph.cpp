8b0df3398c46d051c9941e82031c8c6c07fcdd0f
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-10-27 10:12:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
+++ Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
@@ -4,66 +4,67 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 // User function Template for C++
 class Solution {
   public:
-     void topoSort(int node, vector < pair < int, int >> adj[],
-      int vis[], stack < int > & st) {
-      //This is the function to implement Topological sort. 
-      vis[node] = 1;
-      for (auto it: adj[node]) {
-        int v = it.first;
-        if (!vis[v]) {
-          topoSort(v, adj, vis, st);
-        }
-      }
-      st.push(node);
-    }
-     vector<int> shortestPath(int N,int M, vector<vector<int>>& edges){
-        // Approach-1
-        vector<pair<int,int>> adj[N];
-        vector<int> indegree[N];
-        for (int i = 0; i < M; i++) {
-        int u = edges[i][0];
-        int v = edges[i][1];
-        int wt = edges[i][2];
-        adj[u].push_back({v, wt}); 
-      }
-        int vis[N] = {0};
+    void topoSort(int node, vector<int>& visited, vector<pair<int,int>> adj[], stack<int>& st){
+        visited[node]=1;
         
-        stack < int > st;
-        for (int i = 0; i < N; i++) {
-        if (!vis[i]) {
-          topoSort(i, adj, vis, st);
+        for(auto it: adj[node]){
+            int neighbour = it.first;
+            int weight = it.second;
+            if(visited[neighbour]==0){
+                topoSort(neighbour,visited,adj,st);
+            }
         }
-      }
-      
-      vector < int > dist(N);
-      for (int i = 0; i < N; i++) {
-        dist[i] = 1e9;
-      }
-
-      dist[0] = 0;
-      while (!st.empty()) {
-        int node = st.top();
-        st.pop();
-
-        for (auto it: adj[node]) {
-          int v = it.first;
-          int wt = it.second;
-
-          if (dist[node] + wt < dist[v]) {
-            dist[v] = wt + dist[node];
-          }
+        
+        st.push(node);
+    }
+    vector<int> shortestPath(int V, int E, vector<vector<int>>& edges) {
+        //Approach-1
+        vector<pair<int,int>> adj[V];
+        for(int i=0;i<E;i++){
+            int first = edges[i][0];
+            int second = edges[i][1];
+            int weight = edges[i][2];
+            adj[first].push_back({second,weight});
         }
-      }
-
-      for (int i = 0; i < N; i++) {
-        if (dist[i] == 1e9) dist[i] = -1;
-      }
-      return dist;
-
+        
+        //Perform a topological Sort
+        vector<int> visited(V,0);
+        stack<int> st;
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+                topoSort(i,visited,adj,st);
+            }
+        }
+        
+        
+        //At the top of the stack we have to ensure that we are starting form the node
+        while(st.top()!=0){
+            st.pop();
+        }
+        //Pop elements from the stack and start relaxation of the node
+        vector<int> distance(V,1e9);
+        distance[0]=0;
+        while(!st.empty()){
+            int node = st.top();
+            st.pop();
+            for(auto it:adj[node]){
+                int neighbour = it.first;
+                int weight = it.second;
+                if(distance[node]+weight<distance[neighbour]){
+                    distance[neighbour]=distance[node]+weight;
+                }
+            }
+        }
+        
+        for(int i=0;i<V;i++){
+            if(distance[i]==1e9)distance[i]=-1;
+        }
+        return distance;
     }
 };
 
@@ -76,10 +77,11 @@ int main() {
         int n, m;
         cin >> n >> m;
         vector<vector<int>> edges;
-        for(int i=0; i<m; ++i){
+        for (int i = 0; i < m; ++i) {
             vector<int> temp;
-            for(int j=0; j<3; ++j){
-                int x; cin>>x;
+            for (int j = 0; j < 3; ++j) {
+                int x;
+                cin >> x;
                 temp.push_back(x);
             }
             edges.push_back(temp);
