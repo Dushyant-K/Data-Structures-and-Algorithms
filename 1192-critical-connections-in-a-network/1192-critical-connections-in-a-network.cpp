30d89d1cfeb9fff91d70eaf693f6bd8b2bca9367
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-10-30 00:25:03 +0530

    Time: 267 ms (19.68%), Space: 176.8 MB (57.77%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,43 +1,48 @@
 class Solution {
+    private:
     int timer=1;
-    void dfs(int node, int parent, vector<int>& visited, vector<int> adj[], vector<int>& time,          vector<int>& low, vector<vector<int>>& criticalConnections){
+public:
+    void dfs(int node, int parent, vector<int> adj[], vector<int>& visited, vector<int>& tin, vector<int>& low, vector<vector<int>>& bridges){
         visited[node]=1;
-        time[node]=timer;
-        low[node]=timer;
+        tin[node]=low[node]=timer;
         timer++;
+
         for(auto it:adj[node]){
             if(it==parent)continue;
             if(visited[it]==0){
-                dfs(it,node,visited,adj,time,low,criticalConnections);
+                dfs(it,node,adj,visited,tin,low,bridges);
                 low[node]=min(low[node],low[it]);
-                if(low[it]>time[node]){
-                    criticalConnections.push_back({node, it});
+                if(low[it]>tin[node]){
+                    bridges.push_back({node,it});
                 }
             }
             else{
-                low[node] = min(low[it],low[node]);
+                low[node]=min(low[node],low[it]);
             }
         }
     }
-public:
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-       //Use dfs algorithm
-       vector<int> adj[n];
-       for(auto it:connections){
-        int u = it[0];
-        int v = it[1];
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-       }
-       vector<int> visited(n,0);
-       vector<int> time(n,0);
-       vector<int> low(n,0);
-       vector<vector<int>> critcalConnections;
-       for(int i=0;i<n;i++){
-        if(visited[i]==0){
-            dfs(i,-1,visited,adj,time,low,critcalConnections);
+        //Approach-1
+        vector<int> adj[n];
+        for(auto it:connections){
+            int node1 = it[0];
+            int node2 = it[1];
+            adj[node1].push_back(node2);
+            adj[node2].push_back(node1);
         }
-       }
-       return critcalConnections;
+
+        //Prepare the containers
+        vector<int> visited(n,0);
+        vector<int> tin(n);
+        vector<int> low(n);
+        vector<vector<int>> bridges;
+
+        //Now start the iteration
+        for(int i=0;i<n;i++){
+            if(visited[i]==0){
+                dfs(i,-1,adj,visited,tin,low,bridges);
+            }
+        }
+        return bridges;
     }
 };
