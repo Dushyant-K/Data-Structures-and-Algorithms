ef3fd3d430e3a066a416b6c17f672e29fb23f025
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-04 10:38:59 +0530

    Time: 55 ms (7.69%), Space: 54.2 MB (5.06%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,32 +1,59 @@
 class Solution {
-public:
-    vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> solutions;
-        vector<string> board(n, string(n, '.'));
-        vector<int> leftRow(n, 0), upperDiagonal(2 * n - 1, 0), lowerDiagonal(2 * n - 1, 0);
-        solve(0, n, board, solutions, leftRow, upperDiagonal, lowerDiagonal);
-        return solutions;
+private:
+bool isPossible(int col, int row, int n, vector<string> board){
+    int duprow = row;
+    int dupcol = col;
+
+    //Check for upper diagonal
+    while(duprow>=0&&dupcol>=0){
+        if(board[dupcol][duprow]=='Q')return false;
+        duprow--;
+        dupcol--;
     }
 
-private:
-    void solve(int col, int n, vector<string>& board, vector<vector<string>>& solutions,
-               vector<int>& leftRow, vector<int>& upperDiagonal, vector<int>& lowerDiagonal) {
-        if (col == n) {
-            solutions.push_back(board);
-            return;
+    duprow=row;
+    dupcol=col;
+    //Checking for row
+    while(dupcol>=0){
+        if(board[dupcol][duprow]=='Q')return false;
+        dupcol--;
+    }
+
+    dupcol=col;
+    duprow=row;
+    while(duprow<n&&dupcol>=0){
+        if(board[dupcol][duprow]=='Q'){
+            return false;
         }
-        for (int row = 0; row < n; row++) {
-            if (leftRow[row] == 0 && upperDiagonal[n - 1 + col - row] == 0 && lowerDiagonal[col + row] == 0) {
-                board[row][col] = 'Q';
-                leftRow[row] = 1;
-                upperDiagonal[n - 1 + col - row] = 1;
-                lowerDiagonal[col + row] = 1;
-                solve(col + 1, n, board, solutions, leftRow, upperDiagonal, lowerDiagonal);
-                board[row][col] = '.';
-                leftRow[row] = 0;
-                upperDiagonal[n - 1 + col - row] = 0;
-                lowerDiagonal[col + row] = 0;
-            }
+        dupcol--;
+        duprow++;
+    }
+    return true;
+}
+void solve(int col, int n, vector<string> board, vector<vector<string>>& ans){
+    if(col==n){
+        ans.push_back(board);
+        return;
+    }
+
+    for(int i=0;i<n;i++){
+        if(isPossible(col,i,n,board)){
+            board[col][i]='Q';
+            solve(col+1,n,board,ans);
+            board[col][i]='.';
         }
+    }
+}
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        //Approach-1
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            board[i]=s;
+        }
+        solve(0,n,board,ans);
+        return ans;
     }
 };
