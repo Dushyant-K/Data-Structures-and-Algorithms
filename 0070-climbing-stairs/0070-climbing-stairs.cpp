4a3378cfdfbb067e57fa432a6db2dae933ab0a1e
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-08 07:30:43 +0530

    Time: 0 ms (100%), Space: 7.7 MB (9.45%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -15,7 +15,16 @@ class Solution {
 public:
     int climbStairs(int n) {
       //Approach-1(Memoiztion Method)
+    //   vector<int> dp(n+1,-1);
+    //   return solve(n,dp);  
+
+      //Approach-2(Tabulation Method)
       vector<int> dp(n+1,-1);
-      return solve(n,dp);  
+      dp[0]=1;
+      dp[1]=1;
+      for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+      }
+      return dp[n];
     }
 };
