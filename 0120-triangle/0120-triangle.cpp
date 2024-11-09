6aff0f0d260e9bad068bb0ba07a14017a3d83d00
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-09 12:49:30 +0530

    Time: 0 ms (100%), Space: 11.8 MB (8.7%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -19,6 +19,32 @@ int minimumPathSumUtil(int i, int j, vector<vector<int> > &triangle, int n,
     return dp[i][j] = min(down, diagonal);
 }
 // Function to find the minimum path sum in the given triangle using dynamic programming
+// int minimumPathSum(vector<vector<int> > &triangle, int n) {
+//     // Create a 2D DP (dynamic programming) array to store minimum path sums
+//     vector<vector<int> > dp(n, vector<int>(n, 0));
+
+//     // Initialize the bottom row of dp with the values from the triangle
+//     for (int j = 0; j < n; j++) {
+//         dp[n - 1][j] = triangle[n - 1][j];
+//     }
+
+//     // Iterate through the triangle rows in reverse order
+//     for (int i = n - 2; i >= 0; i--) {
+//         for (int j = i; j >= 0; j--) {
+//             // Calculate the minimum path sum for the current cell
+//             int down = triangle[i][j] + dp[i + 1][j];
+//             int diagonal = triangle[i][j] + dp[i + 1][j + 1];
+
+//             // Store the minimum of the two possible paths in dp
+//             dp[i][j] = min(down, diagonal);
+//         }
+//     }
+
+//     // The top-left cell of dp now contains the minimum path sum
+//     return dp[0][0];
+// }
+
+// Function to find the minimum path sum in the given triangle using dynamic programming
 int minimumPathSum(vector<vector<int> > &triangle, int n) {
     // Create a 2D DP (dynamic programming) array to store minimum path sums
     vector<vector<int> > dp(n, vector<int>(n, 0));
@@ -53,10 +79,17 @@ public:
     // return minimumPathSumUtil(0, 0, triangle, n, dp);
 
     //Tabulation Method(Bottom up dynamic programming)
+    // // Get the number of rows in the triangle
+    // int n = triangle.size();
+
+    // // Call the minimumPathSum function and print the result
+    //  return minimumPathSum(triangle, n);
+
+    //Approach-3(Space optimization)
     // Get the number of rows in the triangle
     int n = triangle.size();
 
     // Call the minimumPathSum function and print the result
-     return minimumPathSum(triangle, n);
+    return minimumPathSum(triangle, n);
     }
 };
