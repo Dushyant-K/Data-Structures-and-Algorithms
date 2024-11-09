19bf6633a6a84eae990d140658b5f11bb3d2eec0
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-09 12:02:57 +0530

    Time: 3 ms (30.99%), Space: 14.8 MB (68.89%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -21,22 +21,44 @@ public:
         // return solve(m-1,n-1,grid,dp);
 
         //Approach-2(Tabulation method-Bottom Up Dynamic Programming Approach)
-        int m =grid.size();
-        int n = grid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0&&j==0)dp[0][0]=grid[0][0];
-                else{
-                    int top = INT_MAX;
-                    int left = INT_MAX;
-                    if(i>0)top = dp[i-1][j];
-                    if(j>0)left = dp[i][j-1];
+        // int m =grid.size();
+        // int n = grid[0].size();
+        // vector<vector<int>> dp(m,vector<int>(n,-1));
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(i==0&&j==0)dp[0][0]=grid[0][0];
+        //         else{
+        //             int top = INT_MAX;
+        //             int left = INT_MAX;
+        //             if(i>0)top = dp[i-1][j];
+        //             if(j>0)left = dp[i][j-1];
 
-                    dp[i][j] = grid[i][j]+min(top,left);
+        //             dp[i][j] = grid[i][j]+min(top,left);
+        //         }
+        //     }
+        // }
+        // return dp[m-1][n-1];
+
+        //Approach-3(Space optimzation)
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<int> prev(n,-1);
+        for(int i=0;i<m;i++){
+            vector<int> temp(n,-1);
+            for(int j=0;j<n;j++){
+                if(i==0&&j==0){
+                    temp[0]=grid[0][0];
+                }
+                else{
+                    int top=INT_MAX,left=INT_MAX;
+                    if(i>0)top = prev[j];
+                    if(j>0)left = temp[j-1];
+
+                    temp[j] = grid[i][j]+min(top,left);
                 }
             }
+            prev = temp;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
