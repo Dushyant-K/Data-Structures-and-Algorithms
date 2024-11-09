01778d54dcb963d18df1e98fbc4fcdd7fcfbcf32
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-09 11:08:37 +0530

    Time: 0 ms (100%), Space: 10.5 MB (66.49%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
     private:
     int solve(int n, int m,vector<vector<int>> obstacleGrid, vector<vector<int>>& dp){
         //Base-case
-        if(n==0&&m==0)return 1;//Reached Destination(Edge casde is there that's why put it after checking for path)
+        if(n==0&&m==0)return 1;//Reached Destination
         if(n<0||m<0||obstacleGrid[n][m]==1)return 0;//There is no path
 
         //Checking that if this subproblem has been solved before
@@ -17,11 +17,11 @@ class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         // Approach-1(Memoization Method-Top Down Dynamic programming)
-        int n = obstacleGrid.size();
-        int m = obstacleGrid[0].size();
-        if(obstacleGrid[0][0]==1)return 0;
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,obstacleGrid,dp);
+        // int n = obstacleGrid.size();
+        // int m = obstacleGrid[0].size();
+        // if(obstacleGrid[0][0]==1)return 0;
+        // vector<vector<int>> dp(n,vector<int>(m,-1));
+        // return solve(n-1,m-1,obstacleGrid,dp);
 
         //Approach-2(Tabulation Method-Bottom up dynamic programming)
         // int n = obstacleGrid.size();
@@ -43,5 +43,30 @@ public:
         //     }
         // }
         // return dp[n-1][m-1];
+
+        //Approach-3(Space optimization)
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        vector<int> prev(m,-1);
+        if(obstacleGrid[0][0]==1)return 0;
+
+        for(int i=0;i<n;i++){
+            vector<int> temp(m);
+            for(int j=0;j<m;j++){
+                if(i==0&&j==0){
+                    prev[0]=1;
+                    temp[0]=1;
+                }
+                else{
+                    int top=0,left=0;
+                    if(i>0&&obstacleGrid[i][j]!=1)top = prev[j];
+                    if(j>0&&obstacleGrid[i][j]!=1)left = temp[j-1];
+
+                    temp[j]=top+left;
+                }
+            }
+            prev = temp;
+        }
+        return prev[m-1];
     }
 };
