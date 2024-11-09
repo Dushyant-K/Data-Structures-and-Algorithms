099080f2f3c9ad0f6e8288e6a45aafbe42ad0637
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-09 11:55:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -15,9 +15,26 @@ class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
         //Approach-1(Memoization method-top Down Dynamic Programming)
-        int m = grid.size();
-        int n = grid[0].size();
+        // int m = grid.size();
+        // int n = grid[0].size();
+        // vector<vector<int>> dp(m,vector<int>(n,-1));
+        // return solve(m-1,n-1,grid,dp);
+
+        //Approach-2(Tabulation method-Bottom Up Dynamic Programming Approach)
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return solve(m-1,n-1,grid,dp);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0&&j==0)dp[0][0]=grid[0][0];
+                else{
+                    int top = INT_MAX;
+                    int left = INT_MAX;
+                    if(i>0)top = dp[i-1][j];
+                    if(j>0)left = dp[i][j-1];
+
+                    dp[i][j] = grid[i][j]+min(top,left);
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
