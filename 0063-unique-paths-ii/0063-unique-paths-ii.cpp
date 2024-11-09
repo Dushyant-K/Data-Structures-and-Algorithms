68fcccd212d91b4026453fdb3ad7fd915a4e9301
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-09 10:57:34 +0530

    Time: 0 ms (100%), Space: 10.6 MB (40.45%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -17,9 +17,30 @@ class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         //Approach-1(Memoization Method-Top Down Dynamic programming)
+        // int n = obstacleGrid.size();
+        // int m = obstacleGrid[0].size();
+        // vector<vector<int>> dp(n,vector<int>(m,-1));
+        // return solve(n-1,m-1,obstacleGrid,dp);
+
+        //Approach-2(Tabulation Method-Bottom up dynamic programming)
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
+        if(obstacleGrid[0][0]==1)return 0;
         vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,obstacleGrid,dp);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0&&j==0)dp[0][0]=1;
+                else{
+                    
+                    int top = 0;
+                    int left=0;
+                    if(i>0&&obstacleGrid[i][j]!=1)top = dp[i-1][j];
+                    if(j>0&&obstacleGrid[i][j]!=1)left = dp[i][j-1];
+
+                    dp[i][j]=top+left;
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
