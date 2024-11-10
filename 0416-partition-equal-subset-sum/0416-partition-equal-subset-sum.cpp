7b78fb1be7dbafcde243af626c8a3450c23978f8
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-10 16:52:12 +0530

    Time: 340 ms (13.45%), Space: 16.5 MB (61.82%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -19,11 +19,34 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         //Approach-1(Memoization Method-Top Down dynamic programming)
+        // int n = nums.size();
+        // int sum=0;
+        // for(int i=0;i<n;i++)sum+=nums[i];
+        // if(sum%2==1)return false;
+        // vector<vector<int>> dp(n,vector<int>(sum+1,-1));
+        // return solve(n-1,sum/2,nums,dp);
+
+        //Approach-2(Tabulation Method- Bottom up dynamic programming)
         int n = nums.size();
         int sum=0;
         for(int i=0;i<n;i++)sum+=nums[i];
         if(sum%2==1)return false;
-        vector<vector<int>> dp(n,vector<int>(sum+1,-1));
-        return solve(n-1,sum/2,nums,dp);
+        int target=sum/2;
+        vector<vector<bool>> dp(n,vector<bool>(target+1,false));
+
+        for(int i=0;i<n;i++)dp[i][0]=true;
+        if(nums[0]<=target)
+        dp[0][nums[0]]=true;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=target;j++){
+                bool notTake = dp[i-1][j];
+                bool take = false;
+                if(j>=nums[i]){
+                    take = dp[i-1][j-nums[i]];
+                }
+                dp[i][j]=take||notTake;
+            }
+        }
+        return dp[n-1][target];
     }
 };
