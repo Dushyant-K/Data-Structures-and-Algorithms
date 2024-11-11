1981319190e2aca871c481c3659b82e69991d37d
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-11 19:29:43 +0530

    Time: 27 ms (41.3%), Space: 26.5 MB (59.05%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -21,21 +21,41 @@ public:
         // return solve(n-1,coins,amount,dp);
 
         //Approach-2(Tabulation Method-Bottom up dynamic programming)
-        int n= coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,0));
-        for(int i=0;i<=amount;i++){
-            if(i%coins[0]==0)dp[0][i]=1;
-        }
+        // int n= coins.size();
+        // vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        // for(int i=0;i<=amount;i++){
+        //     if(i%coins[0]==0)dp[0][i]=1;
+        // }
+        // for(int i=1;i<n;i++){
+        //     for(int j=0;j<=amount;j++){
+        //         long notTake = dp[i-1][j];
+        //         long take=0;
+        //         if(coins[i]<=j){
+        //             take = dp[i][j-coins[i]];
+        //         }
+        //         dp[i][j]=(take+notTake);
+        //     }
+        // }
+        // return dp[n-1][amount];
+
+        //Approach-3(Space optimisation)
+         int n= coins.size();
+         vector<int> prev(amount+1,0);
+         for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0)prev[i]=1;
+         }
         for(int i=1;i<n;i++){
+            vector<int>curr(amount+1,0);
             for(int j=0;j<=amount;j++){
-                long notTake = dp[i-1][j];
+                long notTake = prev[j];
                 long take=0;
                 if(coins[i]<=j){
-                    take = dp[i][j-coins[i]];
+                    take = curr[j-coins[i]];
                 }
-                dp[i][j]=(take+notTake);
+                curr[j]=(take+notTake);
             }
+            prev = curr;
         }
-        return dp[n-1][amount];
+        return prev[amount];
     }
 };
