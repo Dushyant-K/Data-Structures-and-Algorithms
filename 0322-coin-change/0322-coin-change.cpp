04f85e5e9fc83d6f1198bcd29279086e488b592f
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-11 18:31:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,41 @@
+class Solution {
+    private:
+    int solve(int idx, int amount, vector<int> coins, vector<vector<int>>& dp){
+        if(idx==0){
+            if(amount%coins[0]==0)return (amount/coins[0]);
+            else return 1e9;
+        }
+
+        if(dp[idx][amount]!=-1)return dp[idx][amount];
+        //Take/notTake Method
+        int notTake = 0+solve(idx-1,amount,coins);
+        int take = INT_MAX;
+        if(coins[idx]<=amount){
+            take = 1+solve(idx,amount-coins[idx],coins);
+        }
+        return dp[idx][amount]=min(take,notTake);
+    }
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        //Couldn't solve it using Greedy algorithm
+        // sort(coins.begin(),coins.end());
+        // int n = coins.size();
+        // int left=n-1;
+        // int count=0;
+        // while(left>=0&&amount>0){
+        //     while(amount>=coins[left]){
+        //         amount-=coins[left];
+        //         count++;
+        //     }
+        //     left--;
+        // }
+        // return (amount>0)?-1:count;
+
+        //Approach-1(Memoization Top Down dynamic programming)
+        int n = coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int ans=solve(n-1,amount,coins,dp);
+        if(ans==1e9)return -1;
+        return ans;
+    }
+};
