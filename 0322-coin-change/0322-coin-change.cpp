f239b643565eeac133fd74df758c1462331a386c
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-11 18:43:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -32,10 +32,33 @@ public:
         // return (amount>0)?-1:count;
 
         //Approach-1(Memoization Top Down dynamic programming)
+        // int n = coins.size();
+        // vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        // int ans=solve(n-1,amount,coins,dp);
+        // if(ans==1e9)return -1;
+        // return ans;
+
+
+        //Approach-2(tabulation Method-Bottom up dynamic programming)
         int n = coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans=solve(n-1,amount,coins,dp);
-        if(ans==1e9)return -1;
+        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0)dp[0][i]=i/coins[0];
+            else dp[0][i]=1e9;
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                int notTake=0+dp[i-1][j];
+                int take = 1e9;
+                if(coins[i]<=j){
+                    take = 1+dp[i-1][j-coins[i]];
+                }
+                dp[i][j]=min(take,notTake);
+            }
+        }
+        int ans = dp[n-1][amount];
+        if(ans>=1e9)return -1;
         return ans;
     }
 };
