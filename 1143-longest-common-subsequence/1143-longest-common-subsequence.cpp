ad243390abf61832bc629f21128baae6c3581c59
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-13 07:40:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -17,19 +17,34 @@ public:
         // return solve(idx1,idx2,text1,text2,dp);
 
         //Approach-2(Tabulation Method-Bottom up dynamic programming)
+        // int n1=text1.length();
+        // int n2 = text2.length();
+        // vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+        // for(int i=0;i<=n1;i++)dp[i][0]=0;
+        // for(int i=0;i<=n2;i++)dp[0][i]=0;
+
+        // //Main Approach
+        // for(int i=1;i<=n1;i++){
+        //     for(int j=1;j<=n2;j++){
+        //         if(text1[i-1]==text2[j-1])dp[i][j]=1+dp[i-1][j-1];
+        //         else dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+        //     }
+        // }
+        // return dp[n1][n2];  
+
+        //Approach-3(space optimisation)
         int n1=text1.length();
         int n2 = text2.length();
-        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
-        for(int i=0;i<=n1;i++)dp[i][0]=0;
-        for(int i=0;i<=n2;i++)dp[0][i]=0;
-
+        vector<int> prev(n2+1,0);
         //Main Approach
         for(int i=1;i<=n1;i++){
+            vector<int> curr(n2+1,0);
             for(int j=1;j<=n2;j++){
-                if(text1[i-1]==text2[j-1])dp[i][j]=1+dp[i-1][j-1];
-                else dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+                if(text1[i-1]==text2[j-1])curr[j]=1+prev[j-1];
+                else curr[j]=max(prev[j],prev[j-1]);
             }
+            prev=curr;
         }
-        return dp[n1][n2];   
+        return prev[n2]; 
     }
 };
