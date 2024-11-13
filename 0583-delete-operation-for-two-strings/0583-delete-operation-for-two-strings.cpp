6bb70645ca64f78600596390a4142ac1d57eb898
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-13 19:42:37 +0530

    Time: 18 ms (19.02%), Space: 16.2 MB (76.13%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,50 @@
+class Solution {
+    private:
+    int longestCommonSubsequence(string text1, string text2) {
+        //Approach-1(Memoization Method-Top Down Dynamic Programming)
+        // int idx1=text1.length();
+        // int idx2 = text2.length();
+        // vector<vector<int>> dp(idx1+1,vector<int>(idx2+1,-1));
+        // return solve(idx1,idx2,text1,text2,dp);
+
+        //Approach-2(Tabulation Method-Bottom up dynamic programming)
+        // int n1=text1.length();
+        // int n2 = text2.length();
+        // vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+        // for(int i=0;i<=n1;i++)dp[i][0]=0;
+        // for(int i=0;i<=n2;i++)dp[0][i]=0;
+
+        // //Main Approach
+        // for(int i=1;i<=n1;i++){
+        //     for(int j=1;j<=n2;j++){
+        //         if(text1[i-1]==text2[j-1])dp[i][j]=1+dp[i-1][j-1];
+        //         else dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+        //     }
+        // }
+        // return dp[n1][n2];  
+
+        //Approach-3(space optimisation)
+        int n1=text1.length();
+        int n2 = text2.length();
+        vector<int> prev(n2+1,0);
+        //Main Approach
+        for(int i=1;i<=n1;i++){
+            vector<int> curr(n2+1,0);
+            for(int j=1;j<=n2;j++){
+                if(text1[i-1]==text2[j-1])curr[j]=1+prev[j-1];
+                else curr[j]=max(prev[j],curr[j-1]);
+            }
+            prev=curr;
+        }
+        return prev[n2]; 
+    }
+public:
+    int minDistance(string word1, string word2) {
+        int n1=word1.length();
+        int n2=word2.length();
+        int length=longestCommonSubsequence(word1,word2);
+        int l = n1-length;
+        int r = n2-length;
+        return (l+r);
+    }
+};
