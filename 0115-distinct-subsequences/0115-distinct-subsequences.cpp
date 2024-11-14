c7ca366f8327e69c4a87754e8bda0ecd7400459c
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-14 09:01:24 +0530

    Time: 28 ms (69.91%), Space: 42.6 MB (38.28%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -20,22 +20,42 @@ public:
         // return solve(n1-1,n2-1,s,t,dp);
 
         //Tabulation method-(Bottom up dynamic programming)
+        // int n1=s.length();
+        // int n2=t.length();
+        // vector<vector<double>> dp(n1+1,vector<double>(n2+1,0));
+        // for(int i=0;i<=n1;i++)dp[i][0]=1;
+        // for(int i=1;i<=n2;i++)dp[0][i]=0;
+
+        // for(int i=1;i<=n1;i++){
+        //     for(int j=1;j<=n2;j++){
+        //         if(s[i-1]==t[j-1]){
+        //             dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+        //         }
+        //         else{
+        //             dp[i][j]=dp[i-1][j];
+        //         }
+        //     }
+        // }
+        // return (int)dp[n1][n2];
+
+        //Approach-3(Sapce optimization)
         int n1=s.length();
         int n2=t.length();
-        vector<vector<double>> dp(n1+1,vector<double>(n2+1,0));
-        for(int i=0;i<=n1;i++)dp[i][0]=1;
-        for(int i=1;i<=n2;i++)dp[0][i]=0;
-
+        vector<double> prev(n2+1,0);
+        prev[0]=1;
         for(int i=1;i<=n1;i++){
+            vector<double> curr(n2+1,0);
+            curr[0]=1;
             for(int j=1;j<=n2;j++){
                 if(s[i-1]==t[j-1]){
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                    curr[j]=prev[j-1]+prev[j];
                 }
                 else{
-                    dp[i][j]=dp[i-1][j];
+                    curr[j]=prev[j];
                 }
             }
+            prev=curr;
         }
-        return (int)dp[n1][n2];
+        return (int)prev[n2];
     }
 };
