5e1ffca7d579b06eba48b97dcc13931e4279645f
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-14 08:52:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -4,21 +4,38 @@ class Solution {
         if(idx2<0) return 1;//It means i got the string taht i was searching for in s
         if(idx1<0) return 0; //It means that there is no more matching available in s and t is still non empty
         if(dp[idx1][idx2]!=-1)return dp[idx1][idx2];
-        int l=0,r=0;
         if(s[idx1]==t[idx2]){
-             l = solve(idx1-1,idx2-1,s,t,dp)+solve(idx1-1,idx2,s,t,dp);
+             return dp[idx1][idx2]=solve(idx1-1,idx2-1,s,t,dp)+solve(idx1-1,idx2,s,t,dp);
         }
         else{
-             r = solve(idx1-1,idx2,s,t,dp);
+             return dp[idx1][idx2] = solve(idx1-1,idx2,s,t,dp);
         }
-        return dp[idx1][idx2]=l+r;
     }
 public:
     int numDistinct(string s, string t) {
         //Approach-1(Memoization Method-Top Down dynamic programming)
+        // int n1=s.length();
+        // int n2=t.length();
+        // vector<vector<int>> dp(n1,vector<int>(n2,-1));
+        // return solve(n1-1,n2-1,s,t,dp);
+
+        //Tabulation method-(Bottom up dynamic programming)
         int n1=s.length();
         int n2=t.length();
-        vector<vector<int>> dp(n1,vector<int>(n2,-1));
-        return solve(n1-1,n2-1,s,t,dp);
+        vector<vector<long long>> dp(n1+1,vector<long long>(n2+1,0));
+        for(int i=0;i<=n1;i++)dp[i][0]=1;
+        for(int i=1;i<=n2;i++)dp[0][i]=0;
+
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return (int)dp[n1][n2];
     }
 };
