f4acd9cc80816a16e3d5f095ba568d0912ff8217
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-14 17:34:00 +0530

    Time: 4 ms (76.73%), Space: 12.4 MB (82.82%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -25,25 +25,48 @@ public:
         // return solve(n1-1,n2-1,word1,word2,dp);
 
         //Approach-2(Tabulation Method-Bottom up dynamic programming)
+        // int n1=word1.size();
+        // int n2=word2.size();
+        // vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+        // for(int i=0;i<=n1;i++){
+        //     dp[i][0]=i;
+        // }
+        // for(int i=0;i<=n2;i++){
+        //     dp[0][i]=i;
+        // }
+        // for(int i=1;i<=n1;i++){
+        //     for(int j=1;j<=n2;j++){
+        //         if(word1[i-1]==word2[j-1]){
+        //             dp[i][j]=dp[i-1][j-1];
+        //         }
+        //         else{
+        //             dp[i][j]=1+min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1]));
+        //         }
+        //     }
+        // }
+        // return dp[n1][n2];
+
+
+        //Approach-3(Space optimisation)
         int n1=word1.size();
         int n2=word2.size();
-        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
-        for(int i=0;i<=n1;i++){
-            dp[i][0]=i;
-        }
+        vector<int> prev(n2+1,0);
         for(int i=0;i<=n2;i++){
-            dp[0][i]=i;
+            prev[i]=i;
         }
         for(int i=1;i<=n1;i++){
+            vector<int> curr(n2+1,0);
+            curr[0]=i;
             for(int j=1;j<=n2;j++){
                 if(word1[i-1]==word2[j-1]){
-                    dp[i][j]=dp[i-1][j-1];
+                    curr[j]=prev[j-1];
                 }
                 else{
-                    dp[i][j]=1+min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1]));
+                    curr[j]=1+min(prev[j-1],min(prev[j],curr[j-1]));
                 }
             }
+            prev=curr;
         }
-        return dp[n1][n2];
+        return prev[n2];
     }
 };
