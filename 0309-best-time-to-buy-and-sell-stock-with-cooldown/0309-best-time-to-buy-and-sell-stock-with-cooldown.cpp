f739925c9e13d0d1d34be381c84864b5067c94ad
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-15 17:26:39 +0530

    Time: 4 ms (19.96%), Space: 14.6 MB (40.08%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -13,8 +13,27 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         //Approach-1(Memoization Method-Top Down dynamic programming)
+        // int n=prices.size();
+        // vector<vector<int>> dp(n,vector<int>(2,-1));
+        // return solve(0,1,prices,dp);
+
+        //Approach-2(Tabulation Method-Bottom up dynamic programming)
         int n=prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return solve(0,1,prices,dp);
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        //We don't have to write the base case as already initialized to zero
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=1;j++){
+                if(j){
+                    dp[i][j]=max(-prices[i]+dp[i+1][0],0+dp[i+1][1]);
+                }
+                else{
+                    if(i!=n-1)
+                    dp[i][j]=max(prices[i]+dp[i+2][1],0+dp[i+1][0]);
+                    else
+                    dp[i][j]=max(prices[i]+dp[i+1][1],0+dp[i+1][0]);
+                }
+            }
+        }
+        return dp[0][1];
     }
 };
