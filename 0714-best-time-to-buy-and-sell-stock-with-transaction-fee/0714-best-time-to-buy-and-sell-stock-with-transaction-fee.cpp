49962664c1c6ea414311af9059fb04054f95ed31
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-15 20:24:38 +0530

    Time: 98 ms (38.38%), Space: 93.6 MB (36.61%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -18,28 +18,28 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
     //Approach-1(Memoization Method-Top Down dynamic Programming)
-      int n=prices.size();
-      int buy=1;
-      vector<vector<int>> dp(n,vector<int>(2,-1));
-      return solve(0,buy,prices,dp,fee);  
+    //   int n=prices.size();
+    //   int buy=1;
+    //   vector<vector<int>> dp(n,vector<int>(2,-1));
+    //   return solve(0,buy,prices,dp,fee);  
 
     //Approach-2(Tabulation Method-Bottom up dynamic programming)
-    // int n=prices.size();
-    // vector<vector<int>> dp(n+1,vector<int>(2,0));
-    // for(int i=0;i<2;i++)dp[n][i]=0;
-    // for(int i=n-1;i>=0;i--){
-    //     for(int j=0;j<2;j++){
-    //         if(j){
-    //         int profit = max(-prices[i]+dp[i+1][0],0+dp[i+1][1]);
-    //         dp[i][j]=profit;
-    //         }
-    //         else{
-    //         int profit = max(prices[i]+dp[i+1][1],0+dp[i+1][0]);
-    //         dp[i][j]=profit;
-    //     }
-    //     }
-    // }
-    // return dp[0][1];
+    int n=prices.size();
+    vector<vector<int>> dp(n+1,vector<int>(2,0));
+    for(int i=0;i<2;i++)dp[n][i]=0;
+    for(int i=n-1;i>=0;i--){
+        for(int j=0;j<2;j++){
+            if(j){
+            int profit = max(-prices[i]+dp[i+1][0],0+dp[i+1][1]);
+            dp[i][j]=profit;
+            }
+            else{
+            int profit = max(prices[i]-fee+dp[i+1][1],0+dp[i+1][0]);
+            dp[i][j]=profit;
+        }
+        }
+    }
+    return dp[0][1];
 
     //Approach-3(Space optimization)
     // int n=prices.size();
