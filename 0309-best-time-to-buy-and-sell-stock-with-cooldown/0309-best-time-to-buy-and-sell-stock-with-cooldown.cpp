d19ad71bb2df70d4a89f971d127fe6744a914497
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-15 17:30:17 +0530

    Time: 0 ms (100%), Space: 13.9 MB (86.98%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -18,22 +18,51 @@ public:
         // return solve(0,1,prices,dp);
 
         //Approach-2(Tabulation Method-Bottom up dynamic programming)
+        // int n=prices.size();
+        // vector<vector<int>> dp(n+1,vector<int>(2,0));
+        // //We don't have to write the base case as already initialized to zero
+        // for(int i=n-1;i>=0;i--){
+        //     for(int j=0;j<=1;j++){
+        //         if(j){
+        //             dp[i][j]=max(-prices[i]+dp[i+1][0],0+dp[i+1][1]);
+        //         }
+        //         else{
+        //             if(i!=n-1)
+        //             dp[i][j]=max(prices[i]+dp[i+2][1],0+dp[i+1][0]);
+        //             else
+        //             dp[i][j]=max(prices[i]+dp[i+1][1],0+dp[i+1][0]);
+        //         }
+        //     }
+        // }
+        // return dp[0][1];
+
+        //Approach-3(Space optimization)
         int n=prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2,0));
-        //We don't have to write the base case as already initialized to zero
-        for(int i=n-1;i>=0;i--){
-            for(int j=0;j<=1;j++){
-                if(j){
-                    dp[i][j]=max(-prices[i]+dp[i+1][0],0+dp[i+1][1]);
-                }
-                else{
-                    if(i!=n-1)
-                    dp[i][j]=max(prices[i]+dp[i+2][1],0+dp[i+1][0]);
-                    else
-                    dp[i][j]=max(prices[i]+dp[i+1][1],0+dp[i+1][0]);
-                }
+         // Initialize three arrays to track the maximum profit for buying and selling
+    vector<int> cur(2, 0);       // Current maximum profit
+    vector<int> front1(2, 0);    // Maximum profit one step ahead
+    vector<int> front2(2, 0);    // Maximum profit two steps ahead
+    
+    for (int ind = n - 1; ind >= 0; ind--) {
+        for (int buy = 0; buy <= 1; buy++) {
+            int profit;
+
+            if (buy == 0) { // We can buy the stock
+                profit = max(0 + front1[0], -prices[ind] + front1[1]);
             }
+
+            if (buy == 1) { // We can sell the stock
+                profit = max(0 + front1[1], prices[ind] + front2[0]);
+            }
+            
+            cur[buy] = profit;
         }
-        return dp[0][1];
+        
+        // Update the 'front1' and 'front2' arrays with current values
+        front2 = front1;
+        front1 = cur;
+    }
+    
+    return cur[0];
     }
 };
