1aec4c25d28b24065a038ab20730a716e1bf83e9
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-15 08:10:31 +0530

    Time: 8 ms (8.81%), Space: 18.1 MB (21.3%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -18,9 +18,27 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
       //Approach-1(Memoization Method-Top Down dynamic Programming)
-      int n=prices.size();
-      int buy=1;
-      vector<vector<int>> dp(n,vector<int>(2,-1));
-      return solve(0,buy,prices,dp);  
+    //   int n=prices.size();
+    //   int buy=1;
+    //   vector<vector<int>> dp(n,vector<int>(2,-1));
+    //   return solve(0,buy,prices,dp);  
+
+    //Approach-2(Tabulation Method-Bottom up dynamic programming)
+    int n=prices.size();
+    vector<vector<int>> dp(n+1,vector<int>(2,0));
+    for(int i=0;i<2;i++)dp[n][i]=0;
+    for(int i=n-1;i>=0;i--){
+        for(int j=0;j<2;j++){
+            if(j){
+            int profit = max(-prices[i]+dp[i+1][0],0+dp[i+1][1]);
+            dp[i][j]=profit;
+            }
+            else{
+            int profit = max(prices[i]+dp[i+1][1],0+dp[i+1][0]);
+            dp[i][j]=profit;
+        }
+        }
+    }
+    return dp[0][1];
     }
 };
