a097752fe4a83525bf51557fdaac32e91a44f783
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-15 07:46:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    private:
+    int solve(int idx, int buy, vector<int>& prices){
+        if(idx==prices.size()){
+            return 0;
+        }
+        if(dp[idx][buy]!=-1)return dp[idx][buy];
+
+        if(buy){
+            int profit = max(-prices[idx]+solve(idx+1,0,prices),0+solve(idx+1,1,prices));
+            return dp[idx][buy]=profit;
+        }
+        else{
+            int profit = max(prices[idx]+solve(idx+1,1,prices),0+solve(idx+1,0,prices));
+            return dp[idx][buy]=profit;
+        }
+    }
+public:
+    int maxProfit(vector<int>& prices) {
+      //Approach-1(Memoization Method-Top Down dynamic Programming)
+      int n=prices.size();
+      int buy=1;
+      vector<vector<int>> dp(n,vector<int>(2,-1));
+      return solve(0,buy,prices,dp);  
+    }
+};
