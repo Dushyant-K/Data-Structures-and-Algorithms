50f488d15559eebcb91d6a6952ec70ebe7b47968
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-16 07:23:36 +0530

    Time: 291 ms (29.66%), Space: 294.9 MB (19.09%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -21,10 +21,32 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         //Approach-1(Memoization Method-Top down dynamic Programming)
+        // int n=nums.size();
+        // for(int i=0;i<n;i++)mini=min(mini,nums[i]);
+        // vector<vector<int>> dp(n,vector<int>(n+1,-1));
+        // return solve(0,-1,nums,dp,n);
+
+        //Approach-2(Tabulation method-Bottom up dynamic programming)
         int n=nums.size();
-        int mini=0;
-        for(int i=0;i<n;i++)mini=min(mini,nums[i]);
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return solve(0,-1,nums,dp,n);
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+    
+    for(int ind = n-1; ind>=0; ind --){
+        for (int prev_index = ind-1; prev_index >=-1; prev_index --){
+            
+            int notTake = 0 + dp[ind+1][prev_index +1];
+    
+            int take = 0;
+    
+            if(prev_index == -1 || nums[ind] > nums[prev_index]){
+                
+                take = 1 + dp[ind+1][ind+1];
+            }
+    
+            dp[ind][prev_index+1] = max(notTake,take);
+            
+        }
+    }
+    
+    return dp[0][0];
     }
 };
