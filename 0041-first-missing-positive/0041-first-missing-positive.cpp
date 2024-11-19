ac998e89b5a5cdfa9e2d0d254841ade8b8a12a75
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-19 20:06:52 +0530

    Time: 65 ms (17.73%), Space: 78.3 MB (14.63%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -3,7 +3,7 @@ public:
     int firstMissingPositive(vector<int>& nums) {
         //Approach
         int n=nums.size();
-        set<int> st;
+        unordered_set<int> st;
         int missing=1;
         for(int i=0;i<n;i++){
             st.insert(nums[i]);
