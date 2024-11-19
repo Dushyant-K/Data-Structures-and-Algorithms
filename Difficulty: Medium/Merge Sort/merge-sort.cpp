c6602bdb01747ccce05697e6f0027be3ebb80ee5
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-11-19 07:47:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.cpp
+++ Difficulty: Medium/Merge Sort/merge-sort.cpp
@@ -1,84 +1,82 @@
 //{ Driver Code Starts
-#include <stdio.h>
 #include <bits/stdc++.h>
 using namespace std;
 
 
-
-/* Function to print an array */
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
-
-
 // } Driver Code Ends
-class Solution
-{
-    public:
-    void merge(int arr[], int l, int m, int r)
-    {
-         vector<int> res;
-         int i=l;
-         int j = m+1;
-         while(i<=m&&j<=r){
-             if(arr[i]<=arr[j]){
-                 res.push_back(arr[i]);
-                 i++;
-             }
-             else{
-                 res.push_back(arr[j]);
-                 j++;
-             }
-         }
-         while(i<=m){
-             res.push_back(arr[i]);
-             i++;
-         }
-         while(j<=r){
-             res.push_back(arr[j]);
-             j++;
-         }
-         for (int k = 0; k < res.size(); k++) {
-            arr[l + k] = res[k];
+class Solution {
+    private:
+void merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp; // temporary array
+    int left = low;      // starting index of left half of arr
+    int right = mid + 1;   // starting index of right half of arr
+
+    //storing elements in the temporary array in a sorted manner//
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
         }
-         return;
+        else {
+            temp.push_back(arr[right]);
+            right++;
+        }
     }
-    public:
-    void mergeSort(int arr[], int l, int r)
-    {
-        if(l==r)return;
-        //code here
-        int mid = (l+r)/2;
-        mergeSort(arr,l,mid);
-        mergeSort(arr,mid+1,r);
-        merge(arr,l,mid,r);
+
+    // if elements on the left half are still left //
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    //  if elements on the right half are still left //
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // transfering all elements from temporary to arr //
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+}
+  public:
+    void mergeSort(vector<int>& arr, int low, int high) {
+        // Approach-1
+    if (low >= high) return;
+    int mid = (low + high) / 2 ;
+    mergeSort(arr, low, mid);  // left half
+    mergeSort(arr, mid + 1, high); // right half
+    merge(arr, low, mid, high);  // merging sorted halves
     }
 };
 
 //{ Driver Code Starts.
 
-
-int main()
-{
-    int n,T,i;
-
-    scanf("%d",&T);
-
-    while(T--){
-    
-    scanf("%d",&n);
-    int arr[n+1];
-    for(i=0;i<n;i++)
-      scanf("%d",&arr[i]);
-
-    Solution ob;
-    ob.mergeSort(arr, 0, n-1);
-    printArray(arr, n);
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        obj.mergeSort(arr, 0, arr.size() - 1);
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
+
 // } Driver Code Ends
