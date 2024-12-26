394f406bf9af3121202f0f5c98ccc45365f230ab
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2024-12-26 21:19:41 +0530

    Time: 970 ms (6.2%), Space: 11.5 MB (78.96%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,26 +1,22 @@
 class Solution {
     private:
-    int solve(int idx, int currSum, int target, vector<int>& nums, int n, string pattern, set<string>& st){
-        if(idx==n){
-            if(currSum==target&&st.find(pattern)==st.end()){
-                st.insert(pattern);
-                return 1;
-            }
-            return 0;
+     int solve(int idx, int currSum, int target, vector<int>& nums) {
+        if (idx == nums.size()) {
+            return currSum == target ? 1 : 0;
         }
 
-        int l=solve(idx+1,currSum+nums[idx],target,nums,n,pattern+'+'+to_string(nums[idx]),st);
-        
-        int r=solve(idx+1,currSum-nums[idx],target,nums,n,pattern+'-'+to_string(nums[idx]),st);
+        // Add the current number
+        int add = solve(idx + 1, currSum + nums[idx], target, nums);
 
-        return l+r;
+        // Subtract the current number
+        int subtract = solve(idx + 1, currSum - nums[idx], target, nums);
+
+        return add + subtract;
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         //Approach-1
-        int n=nums.size();
-        if(n==0)return 0;
-        set<string> st;
-        return solve(0,0,target,nums,n,"",st);
+        if (nums.empty()) return 0;
+        return solve(0, 0, target, nums);
     }
 };
