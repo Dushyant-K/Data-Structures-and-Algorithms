2e6e5db6287e175d08d9d14c85d45552ed48a4ab
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-01-01 16:25:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two sum -Pairs with 0 Sum/two-sum-pairs-with-0-sum.cpp
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+class Matrix {
+  public:
+    template <class T>
+    static void input(vector<vector<T>> &A, int n, int m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                scanf("%d ", &A[i][j]);
+            }
+        }
+    }
+
+    template <class T>
+    static void print(vector<vector<T>> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            for (int j = 0; j < A[i].size(); j++) {
+                cout << A[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    vector<vector<int>> getPairs(vector<int>& arr) {
+    std::unordered_set<int> seen;
+    std::unordered_set<std::string> uniquePairs;
+    std::vector<std::vector<int>> result;
+
+    for (int num : arr) {
+        if (seen.count(-num)) {
+            // Form the pair in sorted order
+            int a = std::min(num, -num);
+            int b = std::max(num, -num);
+            std::string pairKey = std::to_string(a) + "," + std::to_string(b);
+            
+            if (!uniquePairs.count(pairKey)) {
+                uniquePairs.insert(pairKey);
+                result.push_back({a, b});
+            }
+        }
+        seen.insert(num);
+    }
+
+    // Sort the result vector to ensure sorted order
+    std::sort(result.begin(), result.end());
+    return result;
+    }
+};
+
+//{ Driver Code Starts.
+//Back-end complete function Template for C++
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n = arr.size();
+        // char c;cin>>c;
+        Solution obj;
+        vector<vector<int>> res = obj.getPairs(arr);
+        if (res.size() == 0) {
+            cout << endl;
+        } else
+            Matrix::print(res);
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
