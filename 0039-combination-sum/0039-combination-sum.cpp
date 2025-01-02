b5bf2f43dd9b60516cb35137a2f73fbf73170e08
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-01-02 08:04:15 +0530

    Time: 78 ms (11.31%), Space: 68.2 MB (8.97%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,29 @@
 class Solution {
-public:
-    void uniqueCombinations(int idx, vector<int> candidates, int target, vector<vector<int>>& ans, vector<int> ds){
-        if(idx==candidates.size()){
+    private:
+    void solve(int idx, int target, vector<int> candidates, vector<int> ds, vector<vector<int>> &ans){
+        if(idx==-1){
             if(target==0){
                 ans.push_back(ds);
             }
             return;
         }
 
-        //Picking this element
+        //take/notTake method
+        solve(idx-1,target,candidates,ds,ans);
+        ds.push_back(candidates[idx]);
         if(candidates[idx]<=target){
-            ds.push_back(candidates[idx]);
-            uniqueCombinations(idx,candidates,target-candidates[idx],ans,ds);
-            ds.pop_back();
+        solve(idx,target-candidates[idx],candidates,ds,ans);
+        ds.pop_back();
         }
-        //Not picking this element
-        uniqueCombinations(idx+1,candidates,target,ans,ds);
     }
+public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        //Approach-1
-        vector<vector<int>> ans;
+        //Approach-1(Memoization Method-Top down dynamic programming)
+        int n = candidates.size();
+        vector<vector<int>> dp(n,vector<int>(target+1,-1));
         vector<int> ds;
-        uniqueCombinations(0,candidates,target,ans,ds);
+        vector<vector<int>> ans;
+        solve(n-1,target,candidates,ds,ans);
         return ans;
     }
 };
