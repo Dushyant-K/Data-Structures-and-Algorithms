6745411081c0f8e99cdd1a1500925501b072a42c
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-01-02 08:38:14 +0530

    Time: 0 ms (100%), Space: 10 MB (53.81%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,16 +1,37 @@
 class Solution {
+    private:
+    void solve(int idx, vector<int>& ds, vector<vector<int>>& ans, vector<int>& nums, int n){
+        if(idx==n){
+            ans.push_back(ds);
+            return;
+        }
+
+        //picking up the element
+        ds.push_back(nums[idx]);
+        solve(idx+1,ds,ans,nums,n);
+        ds.pop_back();
+        solve(idx+1,ds,ans,nums,n);
+    }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n = nums.size();
+        //Approach-1(Bit Manipulation Method)
+        // int n = nums.size();
+        // vector<vector<int>> ans;
+        // for(int i=0;i<(1<<n);i++){
+        //     vector<int> temp;
+        //     for(int j=0;j<n;j++){
+        //         if(i&(1<<j))
+        //         temp.push_back(nums[j]);
+        //     }
+        //     ans.push_back(temp);
+        // }
+        // return ans;
+
+        //Approach-2(Recursion backtracking)
+        int n=nums.size();
         vector<vector<int>> ans;
-        for(int i=0;i<(1<<n);i++){
-            vector<int> temp;
-            for(int j=0;j<n;j++){
-                if(i&(1<<j))
-                temp.push_back(nums[j]);
-            }
-            ans.push_back(temp);
-        }
+        vector<int> ds;
+        solve(0,ds,ans,nums,n);
         return ans;
     }
 };
