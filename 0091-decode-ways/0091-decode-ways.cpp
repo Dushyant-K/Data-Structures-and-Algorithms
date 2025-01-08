c95584f939911976f3603e9df069aaeb77974333
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-01-08 17:09:51 +0530

    Time: 0 ms (100%), Space: 9.7 MB (23.86%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    int solve(int idx, string&s, int n){
+    int solve(int idx, string&s, int n, vector<int>& dp){
         if(idx==n)return 1;
         if(s[idx]=='0')return 0;
+        if(dp[idx]!=-1)return dp[idx];
 
         //Don't Club it
-        int left=solve(idx+1,s,n);
+        int left=solve(idx+1,s,n,dp);
         //Club it
         int right=0;
          if (idx < n-1 && stoi(s.substr(idx, 2)) <= 26) {
-            right = solve(idx + 2, s, n);
+            right = solve(idx + 2, s, n,dp);
         }
 
-        return left+right;
+        return dp[idx]=left+right;
     }
     int numDecodings(string s) {
         //Approach-1(Recursion backtracking)
         int n=s.length();
-        return solve(0,s,n);
+        vector<int> dp(n+1,-1);
+        return solve(0,s,n,dp);
     }
 };
