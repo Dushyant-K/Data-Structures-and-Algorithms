ec707025f021b5c640b89ce54a8ee384689e3420
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-01-08 08:40:51 +0530

    Time: 215 ms (14.07%), Space: 160.1 MB (6.92%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -17,12 +17,46 @@ public:
     }
 
     int numSquares(int n) {
+        // vector<int> squares;
+        // for(int i = 1; i * i <= n; i++){
+        //     squares.push_back(i * i);
+        // }
+
+        // vector<vector<int>> dp(squares.size(),vector<int>(n+1,-1));
+        // return solve(squares.size() - 1, squares, n,dp);
+
+        //Approach-2(Tabulation-Bottom up dynamic programming)
         vector<int> squares;
         for(int i = 1; i * i <= n; i++){
             squares.push_back(i * i);
         }
 
-        vector<vector<int>> dp(squares.size(),vector<int>(n+1,-1));
-        return solve(squares.size() - 1, squares, n,dp);
+        vector<vector<int>> dp(squares.size(),vector<int>(n+1));
+        for(int i=0;i<squares.size();i++)dp[i][0]=0;
+
+       // Initialize the first row (using only the first square)
+        for(int j = 1; j <= n; j++) {
+            if(j % squares[0] == 0) {
+                dp[0][j] = j / squares[0];  // Minimum squares to reach j
+            }
+        }
+
+        // Fill the dp table for remaining squares
+        for(int i = 1; i < squares.size(); i++) {
+            for(int j = 1; j <= n; j++) {
+                int take = INT_MAX;
+                if(squares[i] <= j) {
+                    take = dp[i][j - squares[i]];
+                    if (take != INT_MAX) {
+                        take += 1;
+                    }
+                }
+                int not_take = dp[i - 1][j];
+
+                dp[i][j] = min(take, not_take);
+            }
+        }
+
+        return dp[squares.size() - 1][n];
     }
 };
