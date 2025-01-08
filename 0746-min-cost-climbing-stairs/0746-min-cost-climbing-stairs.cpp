1269bc7854a34736ecc0dd59c16d8853a55b47e0
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-01-08 07:42:52 +0530

    Time: 0 ms (100%), Space: 17.7 MB (38.14%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -11,10 +11,29 @@ public:
     }
     int minCostClimbingStairs(vector<int>& cost) {
         //Approach-1(Top-Down dynamic programming)
+        // int n=cost.size();
+        // cost.resize(n+1);
+        // cost[n]=0;
+        // vector<int> dp(n+1,-1);
+        // return solve(n,cost,dp);
+
+        //Approach-2(Tabulation Method-Bottom up dynamic programming)
         int n=cost.size();
         cost.resize(n+1);
         cost[n]=0;
-        vector<int> dp(n+1,-1);
-        return solve(n,cost,dp);
+        vector<int> dp(n+1,0);
+
+        //Base-Cases
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+
+        for(int i=2;i<=n;i++){
+            int step1=cost[i]+dp[i-1];
+            int step2=cost[i]+dp[i-2];
+
+            dp[i]=min(step1,step2);
+        }
+
+        return dp[n];
     }
 };
