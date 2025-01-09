1bfbdceff297e387ece4f65c95abbc74d54ea079
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-01-09 07:35:33 +0530

    Time: 0 ms (100%), Space: 15.8 MB (77.72%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -13,17 +13,43 @@ public:
         return dp[idx]=max(take,not_take);
     }
     int deleteAndEarn(vector<int>& nums) {
-       //Approach-1(Recursion backtracking)
-       int n=nums.size();
-       int maxi=*max_element(nums.begin(),nums.end());
+       //Approach-1(Recursion backtracking-Memoization Method-Top Down dynamic programming)
+    //    int n=nums.size();
+    //    int maxi=*max_element(nums.begin(),nums.end());
 
-       vector<int> points(maxi+1,0);
+    //    vector<int> points(maxi+1,0);
 
-       for(auto it:nums){
-        points[it]+=it;
-       } 
+    //    for(auto it:nums){
+    //     points[it]+=it;
+    //    } 
        
-       vector<int> dp(maxi+1,-1);
-       return solve(maxi,points,dp);
+    //    vector<int> dp(maxi+1,-1);
+    //    return solve(maxi,points,dp);
+
+    //Approach-2(Tabulation-Bottom up dynamic programming)
+    int n=nums.size();
+
+    int maxi=*max_element(nums.begin(),nums.end());
+
+    vector<int> points(maxi+1,0);
+
+    for(auto it:nums){
+        points[it]+=it;
+    }
+
+    vector<int> dp(maxi+1,0);
+
+    dp[0]=0;
+
+    for(int i=1;i<=maxi;i++){
+        int take=points[i];
+        if(i>1)take+=dp[i-2];
+
+        int not_take=dp[i-1];
+
+        dp[i]=max(take,not_take);
+    }
+
+    return dp[maxi];
     }
 };
