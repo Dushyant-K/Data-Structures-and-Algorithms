970fadaa72e328b8450f522a79607a9648cae13e
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-01-11 08:00:30 +0530

    Time: 15 ms (27.27%), Space: 15.2 MB (21.68%) - LeetHub

--- 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
     bool canConstruct(string s, int k) {
-         int n=s.size();
-        if (n<k) return 0;
-        bitset<26> freq=0;
-        for(char c: s)
-            freq.flip(c-'a');
-        return freq.count()<=k;
+    //Approach-1(Uses a property of palindrome)
+    
+     // If the string length is less than k, we can't form k palindromes
+    if (s.size() < k) return false;
+
+    // Count the frequency of each character
+    unordered_map<char, int> freq;
+    for (char c : s) {
+        freq[c]++;
+    }
+
+    // Count characters with odd frequencies
+    int oddCount = 0;
+    for (auto& [key, count] : freq) {
+        if (count % 2 != 0) {
+            oddCount++;
+        }
+    }
+
+    // Check if the number of odd frequencies is less than or equal to k
+    return oddCount <= k;
     }
 };
