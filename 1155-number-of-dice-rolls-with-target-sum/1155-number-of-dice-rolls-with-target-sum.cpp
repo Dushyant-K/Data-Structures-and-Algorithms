bb19eec9edf6d9820f5e5e54d1dc6da483701726
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-01-18 07:57:24 +0530

    Time: 17 ms (43.67%), Space: 11.2 MB (51.32%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -6,7 +6,7 @@ class Solution {
             return target==0;
         }
         if(dp[n][target]!=-1)return dp[n][target];
-        
+
         int ways=0;
         for(int i=1;i<=k;i++){
             if(i<=target){
@@ -18,8 +18,27 @@ class Solution {
     }
 public:
     int numRollsToTarget(int n, int k, int target) {
-       //Approach-1(Memoization Method-top down dynamic programming)
-       vector<vector<int>> dp(n+1,vector<int>(target+1,-1));
-       return solve(n,k,target,dp); 
+    //Approach-1(Memoization Method-top down dynamic programming)
+    //    vector<vector<int>> dp(n+1,vector<int>(target+1,-1));
+    //    return solve(n,k,target,dp); 
+
+    //Approach-2(Tabulation Method-Bottom up dynamic programming)
+    vector<vector<int>> dp(n+1,vector<int>(target+1,0));
+    dp[0][0]=1;
+
+    for(int i=1;i<=n;i++){
+        for(int targets=0;targets<=target;targets++){
+            int ways=0;
+            for(int j=1;j<=k;j++){
+                if(j<=targets){
+                    ways=(ways+dp[i-1][targets-j])%mod;
+                }
+                else break;
+            }
+            dp[i][targets]=ways;
+        }
+    }
+
+    return dp[n][target];
     }
 };
