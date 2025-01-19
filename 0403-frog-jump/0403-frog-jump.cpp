5d199369162612836b807bf2b75fead40fd9e25b
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-01-19 18:01:19 +0530

    Time: 94 ms (60.16%), Space: 238.9 MB (13.14%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,35 +1,35 @@
 class Solution {
-private:
-bool canCrossHelper(vector<int>& stones, int pos, int lastJump, vector<vector<int>>& dp, unordered_map<int, int>& stoneIndex) {
+    private:
+    bool canCrossHelper(vector<int>& stones, int currentIndex, int lastJump, vector<vector<int>>& dp, unordered_map<int, int>& stoneIndex) {
     // If the frog has reached the last stone
-    if (pos == stones.back()) {
+    if (currentIndex == stones.size() - 1) {
         return true;
     }
 
     // If the current state is already computed
-    if (dp[stoneIndex[pos]][lastJump] != -1) {
-        return dp[stoneIndex[pos]][lastJump];
+    if (dp[currentIndex][lastJump] != -1) {
+        return dp[currentIndex][lastJump];
     }
 
     // Try jumps of k-1, k, and k+1 units
     for (int jump = lastJump - 1; jump <= lastJump + 1; ++jump) {
         if (jump > 0) { // Only consider positive jumps
-            int nextPos = pos + jump;
+            int nextPos = stones[currentIndex] + jump;
 
             // Check if the next position is a stone
             if (stoneIndex.count(nextPos)) {
-                if (canCrossHelper(stones, nextPos, jump, dp, stoneIndex)) {
-                    return dp[stoneIndex[pos]][lastJump] = 1;
+                int nextIndex = stoneIndex[nextPos];
+                if (canCrossHelper(stones, nextIndex, jump, dp, stoneIndex)) {
+                    return dp[currentIndex][lastJump] = 1;
                 }
             }
         }
     }
 
-    return dp[stoneIndex[pos]][lastJump] = 0;
+    return dp[currentIndex][lastJump] = 0;
 }
 public:
     bool canCross(vector<int>& stones) {
-    //Approach-1(Memoization Method=Top down dynamic programming)
     int n = stones.size();
     vector<vector<int>> dp(n, vector<int>(n, -1)); // DP table to store results
     unordered_map<int, int> stoneIndex; // Map stone position to its index
@@ -42,6 +42,6 @@ public:
         return false; // If the first jump isn't possible
     }
 
-    return canCrossHelper(stones, 0, 0, dp, stoneIndex); 
+    return canCrossHelper(stones, 0, 0, dp, stoneIndex);
     }
 };
