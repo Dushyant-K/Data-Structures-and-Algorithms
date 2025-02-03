55b0910383f741a82ab5aa47e1e5fa2b9079ed4f
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-02-03 19:02:41 +0530

    Time: 0 ms (100%), Space: 28 MB (27.06%) - LeetHub

--- 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -2,36 +2,64 @@ class Solution {
 public:
     int longestMonotonicSubarray(vector<int>& nums) {
         //Approach-1
-        int n=nums.size();
-        int strictlyIncreasing=1;
+        // int n=nums.size();
+        // int strictlyIncreasing=1;
 
-        int currLength=1;
+        // int currLength=1;
+        // for(int i=1;i<n;i++){
+        //     if(nums[i]>nums[i-1]){
+        //         currLength++;
+        //     }
+        //     else{
+        //         strictlyIncreasing=max(strictlyIncreasing,currLength);
+        //         currLength=1;
+        //     }
+        // }
+        // strictlyIncreasing=max(strictlyIncreasing,currLength);
+
+
+        // int strictlyDecreasing=1;
+
+        // currLength=1;
+        // for(int i=1;i<n;i++){
+        //     if(nums[i]<nums[i-1]){
+        //         currLength++;
+        //     }
+        //     else{
+        //         strictlyDecreasing=max(strictlyDecreasing,currLength);
+        //         currLength=1;
+        //     }
+        // }
+        // strictlyDecreasing=max(strictlyDecreasing,currLength);
+
+        // return max(strictlyIncreasing,strictlyDecreasing);
+
+        //Approach-2(Optimal Approach)
+        int n=nums.size();
+        int increasing=1;
+        int decreasing=1;
+
+        int maxi=1;
         for(int i=1;i<n;i++){
             if(nums[i]>nums[i-1]){
-                currLength++;
+                maxi=max(maxi,decreasing);
+                decreasing=1;
+                increasing++;
+            }
+            else if(nums[i]<nums[i-1]){
+                maxi=max(maxi,increasing);
+                increasing=1;
+                decreasing++;
             }
             else{
-                strictlyIncreasing=max(strictlyIncreasing,currLength);
-                currLength=1;
+                maxi=max(maxi,increasing);
+                maxi=max(maxi,decreasing);
+                increasing=1;
+                decreasing=1;
             }
         }
-        strictlyIncreasing=max(strictlyIncreasing,currLength);
-
-
-        int strictlyDecreasing=1;
-
-        currLength=1;
-        for(int i=1;i<n;i++){
-            if(nums[i]<nums[i-1]){
-                currLength++;
-            }
-            else{
-                strictlyDecreasing=max(strictlyDecreasing,currLength);
-                currLength=1;
-            }
-        }
-        strictlyDecreasing=max(strictlyDecreasing,currLength);
-
-        return max(strictlyIncreasing,strictlyDecreasing);
+        maxi=max(maxi,increasing);
+        maxi=max(maxi,decreasing);
+        return maxi;
     }
 };
