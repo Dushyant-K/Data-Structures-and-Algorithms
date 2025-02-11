aee7a7bafe9979fe6bc7f13b646a04dea8ee73d2
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-02-11 22:44:05 +0530

    Time: 19 ms (5.85%), Space: 28 MB (5.32%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,19 +1,51 @@
 class Solution {
+    private:
+    bool isValid(stack<char> st, string part, int n){
+
+        while(n>0){
+            if(st.top()!=part[n-1])return false;
+            st.pop();
+            n--;
+        }
+        return true;
+    }
 public:
     string removeOccurrences(string s, string part) {
         //Approach-1(Brute force)
+        // int n=s.length();
+        // int m=part.length();
+
+        // while(true){
+        //     int idx=s.find(part);
+
+        //     if(idx==string::npos){
+        //         break;
+        //     }
+        //     s.erase(idx,m);
+        // }
+
+        // return s;
+
+        //Approach-2
         int n=s.length();
         int m=part.length();
 
-        while(true){
-            int idx=s.find(part);
+        stack<char> st;
 
-            if(idx==string::npos){
-                break;
+        for(int i=0;i<n;i++){
+            st.push(s[i]);
+            if(st.size()>=m&& isValid(st,part,m)){
+                for(int i=0;i<m;i++)st.pop();
             }
-            s.erase(idx,m);
         }
 
-        return s;
+        string result="";
+        while(!st.empty()){
+            result.push_back(st.top());
+            st.pop();
+        }
+
+        reverse(result.begin(),result.end());
+        return result;
     }
 };
