1d55769f40c6b5e6b91ee55b5e5a3087dccf19fd
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-02-20 16:41:11 +0530

    Time: 0 ms (100%), Space: 12.7 MB (69.88%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        //Cantor's Diagonization
+        int n=nums.size();
+        string ans="";
+
+        for(int i=0;i<n;i++){
+            ans+=(nums[i][i]=='0')?'1':'0';
+        }
+        return ans;
+    }
+};
