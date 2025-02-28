30a588c973beca89a38f08c8d5c3e4f5d47c596f
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-02-28 08:45:35 +0530

    Time: 304 ms (50.93%), Space: 15.7 MB (81.16%) - LeetHub

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
@@ -1,33 +1,36 @@
-int dp[1000][1000];
 class Solution {
+    private:
+    int solve(int i, int j, unordered_map<int,int>& mpp, vector<int>& arr){
+        int target=arr[j]-arr[i];
+
+        if(mpp.find(target)!=mpp.end()&&mpp[target]<i){
+            int k=mpp[target];
+            return 1+solve(k,i,mpp,arr);
+        }
+
+        return 2;
+    }
 public:
-    static int lenLongestFibSubseq(vector<int>& arr) {
-        const int n=arr.size();
-        unordered_map<int, int> x2i;
-        x2i.reserve(n);
-        fill(&dp[0][0], &dp[0][0]+n*1000, 2);
-        int ans=0;
-        
-        // Step 1: Store indices of numbers in arr
-        for (int i=0; i < n; i++) 
-            x2i[arr[i]]=i;
+    int lenLongestFibSubseq(vector<int>& arr) {
+        //Approach-1(Recursion)
+        int n=arr.size();
+        unordered_map<int,int> mpp;
 
-        // Step 2: Iterate over pairs and compute DP
-        for (int i1=1; i1<n-1; i1++) {
-            int f1=arr[i1];
-            for (int i2=i1+1; i2<n; i2++) {
-                int f2=arr[i2];
-                int f0=f2-f1;  // Fibonacci first term
-                if (f0>=f1) break; //impossible for tuple (f0, f1, f2)
-                if (x2i.count(f0)) {  //Found
-                    int i0=x2i[f0];
-                    dp[i1][i2]=dp[i0][i1]+1;
+        for(int i=0;i<n;i++){
+            mpp[arr[i]]=i;//arr[i] -> index
+        }
+
+        int result=0;
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int length=solve(i,j,mpp,arr);
+                if(length>=3){
+                    result=max(result,length);
                 }
-
-                ans=max(ans, dp[i1][i2]);  //Track the max length
             }
         }
 
-        return ans>2?ans:0;  //length>2
+        return result;
     }
 };
