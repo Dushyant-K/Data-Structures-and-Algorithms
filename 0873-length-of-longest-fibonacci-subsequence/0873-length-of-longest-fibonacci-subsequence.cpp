af1127c2703d3d07ba2cfaf7f7d6e1a4e498d9dc
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-02-28 08:48:06 +0530

    Time: 413 ms (13.25%), Space: 150 MB (21.33%) - LeetHub

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
@@ -1,11 +1,13 @@
 class Solution {
     private:
-    int solve(int i, int j, unordered_map<int,int>& mpp, vector<int>& arr){
+    int solve(int i, int j, unordered_map<int,int>& mpp, vector<int>& arr, vector<vector<int>>& dp){
+        if(dp[i][j]!=-1)return dp[i][j];
+
         int target=arr[j]-arr[i];
 
         if(mpp.find(target)!=mpp.end()&&mpp[target]<i){
             int k=mpp[target];
-            return 1+solve(k,i,mpp,arr);
+            return dp[i][j]=1+solve(k,i,mpp,arr,dp);
         }
 
         return 2;
@@ -13,8 +15,30 @@ class Solution {
 public:
     int lenLongestFibSubseq(vector<int>& arr) {
         //Approach-1(Recursion)
+        // int n=arr.size();
+        // unordered_map<int,int> mpp;
+
+        // for(int i=0;i<n;i++){
+        //     mpp[arr[i]]=i;//arr[i] -> index
+        // }
+
+        // int result=0;
+
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         int length=solve(i,j,mpp,arr);
+        //         if(length>=3){
+        //             result=max(result,length);
+        //         }
+        //     }
+        // }
+
+        // return result;
+
+        //Approach-2(Memoizaion Method- Top Down dynaic progeamming)
         int n=arr.size();
         unordered_map<int,int> mpp;
+        vector<vector<int>> dp(n,vector<int>(n,-1));
 
         for(int i=0;i<n;i++){
             mpp[arr[i]]=i;//arr[i] -> index
@@ -24,7 +48,7 @@ public:
 
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                int length=solve(i,j,mpp,arr);
+                int length=solve(i,j,mpp,arr,dp);
                 if(length>=3){
                     result=max(result,length);
                 }
