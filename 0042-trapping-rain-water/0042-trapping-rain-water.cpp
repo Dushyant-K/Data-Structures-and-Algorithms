c8e76211927a55287cf1b7c4ab382a1ffe613dd4
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-03-01 11:12:07 +0530

    Time: 0 ms (100%), Space: 25.8 MB (99.61%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,23 +1,49 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-        int prefixMax[n];
-        int suffixMax[n];
-        prefixMax[0] = height[0];
-        for(int i=1;i<n;i++){
-            prefixMax[i] = max(prefixMax[i-1],height[i]);
-        }
-        suffixMax[n-1] = height[n-1];
-        for(int i=n-2;i>=0;i--){
-            suffixMax[i] = max(suffixMax[i+1],height[i]);
-        }
-        int volume=0;
-        for(int i=0;i<=n-1;i++){
-            if((prefixMax[i]>height[i])&&(suffixMax[i]>height[i])){
-                volume+=min(prefixMax[i],suffixMax[i])-height[i];
-            }
-        }
-        return volume;
+        //Approach-1
+        // int n = height.size();
+        // int prefixMax[n];
+        // int suffixMax[n];
+        // prefixMax[0] = height[0];
+        // for(int i=1;i<n;i++){
+        //     prefixMax[i] = max(prefixMax[i-1],height[i]);
+        // }
+        // suffixMax[n-1] = height[n-1];
+        // for(int i=n-2;i>=0;i--){
+        //     suffixMax[i] = max(suffixMax[i+1],height[i]);
+        // }
+        // int volume=0;
+        // for(int i=0;i<=n-1;i++){
+        //     if((prefixMax[i]>height[i])&&(suffixMax[i]>height[i])){
+        //         volume+=min(prefixMax[i],suffixMax[i])-height[i];
+        //     }
+        // }
+        // return volume;
+
+
+    //Approach-2
+    int n = height.size();
+    int left = 0, right = n - 1;
+    int res = 0;
+    int maxLeft = 0, maxRight = 0;
+    while (left <= right) {
+    if (height[left] <= height[right]) {
+      if (height[left] >= maxLeft) {
+        maxLeft = height[left];
+      } else {
+        res += maxLeft - height[left];
+      }
+      left++;
+    } else {
+      if (height[right] >= maxRight) {
+        maxRight = height[right];
+      } else {
+        res += maxRight - height[right];
+      }
+      right--;
+    }
+    }
+    return res;
     }
 };
