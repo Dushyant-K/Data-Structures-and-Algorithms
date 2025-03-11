d06bfc4be288aac08583942400c0f32cad67999d
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-03-11 22:44:40 +0530

    Time: 6 ms (73.15%), Space: 11 MB (57.3%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -2,16 +2,30 @@ class Solution {
 public:
     int numberOfSubstrings(string s) {
         //Approach-1
-        int n =s.length();
+        // int n =s.length();
+        // int count=0;
+        // for(int i=0;i<n;i++){
+        //     int hash[3]={0};
+        //     for(int j=i;j<n;j++){
+        //         hash[s[j]-'a']++;
+        //         if((hash[0]!=0)&&(hash[1]!=0)&&(hash[2]!=0)){
+        //             count+=n-j;
+        //             break;
+        //         }
+        //     }
+        // }
+        // return count;
+
+        //Approach-2
         int count=0;
-        for(int i=0;i<n;i++){
-            int hash[3]={0};
-            for(int j=i;j<n;j++){
-                hash[s[j]-'a']++;
-                if((hash[0]!=0)&&(hash[1]!=0)&&(hash[2]!=0)){
-                    count+=n-j;
-                    break;
-                }
+        int hash[3];
+        for(int i=0;i<=2;i++){
+            hash[i]=-1;
+        }
+        for(int i=0;i<s.length();i++){
+            hash[s[i]-'a']=i;
+            if((hash[0]!=-1)&&(hash[1]!=-1)&&(hash[2]!=-1)){
+                count+=min(hash[0],min(hash[1],hash[2]))+1;
             }
         }
         return count;
