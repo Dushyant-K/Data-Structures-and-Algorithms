0bc94e54d39faf2776a4ac3692b3932105c929bf
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-03-31 08:20:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.cpp
+++ Difficulty: Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.cpp
@@ -1,37 +1,35 @@
 //{ Driver Code Starts
-// Initial Template for C++
-
 #include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
+
 /*You are required to complete this function*/
 
 class Solution {
   public:
-    int maxLen(vector<int>& arr, int n) {
-        // Approach-1
+    int maxLen(vector<int>& arr) {
+        // Approach-1(Prefix Sum)
+        int n=arr.size();
+        unordered_map<int,int> mpp;
+        
         int prefixSum=0;
-        map<int,int> preSumMap;
-        int length = 0;
+        int longest=0;
+        mpp[0]=-1;
         for(int i=0;i<n;i++){
             prefixSum+=arr[i];
-            if(prefixSum==0){
-                length = max(length,i+1);
-            }
-            else{
-                if(preSumMap.find(prefixSum)!=preSumMap.end()){
-                length = max(length,i-preSumMap[prefixSum]);
-            }
-            else{
-                preSumMap[prefixSum]=i;
-                }
+            
+            if(mpp.find(prefixSum)!=mpp.end()){
+                longest=max(longest,i-mpp[prefixSum]);
+            }else{
+                mpp[prefixSum]=i;
             }
         }
-        return length;
+        return longest;
     }
 };
+
 
 
 //{ Driver Code Starts.
@@ -39,16 +37,24 @@ class Solution {
 int main() {
     int t;
     cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
     while (t--) {
-        int m;
-        cin >> m;
-        vector<int> array1(m);
-        for (int i = 0; i < m; ++i) {
-            cin >> array1[i];
-        }
-        Solution ob;
-        cout << ob.maxLen(array1, m) << endl;
+        int n;
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.maxLen(a) << endl;
+        cout << "~\n";
     }
+
     return 0;
 }
 
