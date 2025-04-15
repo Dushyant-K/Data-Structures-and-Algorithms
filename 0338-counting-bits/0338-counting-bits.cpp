2fba1ae3f8d6cbadbcff18b58c070cbd2b1f5a83
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-04-15 08:23:01 +0530

    Time: 0 ms (100%), Space: 10.7 MB (38.72%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -15,10 +15,23 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         //Approach-1
-        vector<int> ans(n+1);
+        // vector<int> ans(n+1);
 
-        for(int i=0;i<=n;i++){
-            ans[i] = helper(i);
+        // for(int i=0;i<=n;i++){
+        //     ans[i] = helper(i);
+        // }
+
+        // return ans;
+
+        //Approach-2(Optimal Approach)
+        vector<int> ans(n+1,0);
+
+        for(int i=1;i<=n;i++){
+            if(i%2==0){
+                ans[i]=ans[i/2];
+            }else{
+                ans[i]=ans[i/2]+1;
+            }
         }
 
         return ans;
