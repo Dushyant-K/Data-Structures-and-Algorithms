c1d2a363c755ff92bee4bff421391524f9770d1e
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-04-15 10:26:40 +0530

    Time: 0 ms (100%), Space: 13.3 MB (65.32%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -17,13 +17,25 @@ public:
         // return ans;
 
         //Approach-2(Sorting)
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
+        // int n=nums.size();
+        // sort(nums.begin(),nums.end());
 
-        for(int i=1;i<n;i+=3){
-            if(nums[i]!=nums[i-1])return nums[i-1];
+        // for(int i=1;i<n;i+=3){
+        //     if(nums[i]!=nums[i-1])return nums[i-1];
+        // }
+
+        // return nums[n-1];
+
+        //Approach-3(Bucket Method)
+        int n = nums.size();
+        int ones=0;
+        int twos=0;
+
+        for(int i=0;i<n;i++){
+            ones = (ones^nums[i])&(~twos);
+            twos = (twos^nums[i])&(~ones);
         }
 
-        return nums[n-1];
+        return ones;
     }
 };
