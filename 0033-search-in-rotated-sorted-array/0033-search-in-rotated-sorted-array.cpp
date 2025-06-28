21ba4c4d7e62471b9e679cef30dd28b4e507598d
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-06-28 09:56:03 +0530

    Time: 0 ms (100%), Space: 15.3 MB (32.62%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -7,10 +7,9 @@ public:
         int ans=-1;
         while(low<=high){
             int mid=(low+high)/2;
-            if(nums[mid]==target)return mid;
-           
-           if(nums[low]<=nums[mid]){
-            if(nums[low]<=target && nums[mid]>target){
+            if(nums[mid]==target)return mid;         
+            if(nums[low]<=nums[mid]){
+            if(nums[low]<=target && nums[mid]>=target){
                 high=mid-1;
             }
             else{
@@ -18,13 +17,12 @@ public:
             }
            }
            else{
-            if(nums[mid]<target && nums[high]>=target){
+            if(nums[mid]<=target && nums[high]>=target){
                 low=mid+1;
             }
             else{
                 high=mid-1;
             }
-
            }
         }
         return -1;      
