0048744d3714bb80f1b19d5502616a938e3a2836
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-07-07 18:26:03 +0530

    Time: 9 ms (28.94%), Space: 16.3 MB (37.42%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,29 +1,54 @@
 class Solution {
-    private:
-    void solve(int idx, int target, vector<int> candidates, vector<int> ds, vector<vector<int>> &ans){
-        if(idx==-1){
+public:
+    void uniqueCombinations(int idx, vector<int> candidates, int target, vector<vector<int>>& ans, vector<int> ds){
+        if(idx==candidates.size()){
             if(target==0){
                 ans.push_back(ds);
             }
             return;
         }
 
-        //take/notTake method
-        solve(idx-1,target,candidates,ds,ans);
-        ds.push_back(candidates[idx]);
+        //Picking this element
         if(candidates[idx]<=target){
-        solve(idx,target-candidates[idx],candidates,ds,ans);
-        ds.pop_back();
+            ds.push_back(candidates[idx]);
+            uniqueCombinations(idx,candidates,target-candidates[idx],ans,ds);
+            ds.pop_back();
+        }
+        //Not picking this element
+        uniqueCombinations(idx+1,candidates,target,ans,ds);
+    }
+    void uniqueCombinations1(int idx, vector<int>& candidates, vector<int> ds, vector<vector<int>>& ans, int target, int n){
+        if(target==0){
+            ans.push_back(ds);
+            return;
+        }
+
+        for(int i=idx;i<n;i++){
+            if(candidates[i]<=target){
+                ds.push_back(candidates[i]);
+                uniqueCombinations1(i,candidates,ds,ans,target-candidates[i],n);
+                ds.pop_back();
+            }
+            else{
+                return;
+            }
         }
     }
-public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        //Approach-1(Memoization Method-Top down dynamic programming)
+        //Approach-1
+        // vector<vector<int>> ans;
+        // vector<int> ds;
+        // uniqueCombinations(0,candidates,target,ans,ds);
+        // return ans;
+
+        //Approach-2(Using Recursion after sorting)
         int n = candidates.size();
-        vector<vector<int>> dp(n,vector<int>(target+1,-1));
-        vector<int> ds;
         vector<vector<int>> ans;
-        solve(n-1,target,candidates,ds,ans);
+        vector<int> ds;
+
+        sort(candidates.begin(),candidates.end());
+        uniqueCombinations1(0,candidates,ds,ans,target,n);
+
         return ans;
     }
 };
