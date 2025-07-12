f03a05f38f20503954b24e8c0cfbb950ef40ed2e
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-07-12 09:50:10 +0530

    Time: 0 ms (100%), Space: 21 MB (24.33%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,30 +1,35 @@
 class Solution {
-public: 
-    void dfs(int node, vector<int>& visited, vector<int> adj[]){
+    private:
+    void dfsTraversal(int node, vector<int> adj[], vector<int>& visited){
         visited[node]=1;
-        for(auto it:adj[node]){
-            if(visited[it]==0){
-                dfs(it,visited,adj);
+
+        for(auto it: adj[node]){
+            if(!visited[it]){
+                dfsTraversal(it,adj,visited);
             }
         }
     }
+public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        //Approach-1
+        //Approach-1(Using DFS Algorithim)
         int n = isConnected.size();
-        vector<int> adj[n];
+        vector<int> adj[n+1];
+
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(isConnected[i][j]==1){
                     adj[i].push_back(j);
+                    adj[j].push_back(i);
                 }
             }
         }
+
+        vector<int> visited(n+1,0);
         int provinces=0;
-        vector<int> visited(n,0);
         for(int i=0;i<n;i++){
             if(visited[i]==0){
+                dfsTraversal(i,adj,visited);
                 provinces++;
-                dfs(i,visited,adj);
             }
         }
         return provinces;
