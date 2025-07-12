eb5e1f76444ebcf8fd5df86ba2a123f5b094a2b1
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-07-12 10:06:52 +0530

    Time: 0 ms (100%), Space: 16.1 MB (100%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -10,34 +10,35 @@ public:
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
                     visited[i][j]=2;
-                      q.push({{i,j},0});
+                    q.push({{i,j},0});
                 }
                 else{
                     visited[i][j]=0;
                 }
             }
         }
-        int drow[]={0,1,0,-1};
-        int dcol[] = {1,0,-1,0};
         int t=0;
+        int adjRow[] = {0,1,0,-1};
+        int adjCol[] = {1,0,-1,0};
         while(!q.empty()){
             int row = q.front().first.first;
             int col = q.front().first.second;
             int time = q.front().second;
             q.pop();
             t = max(t,time);
-            for(int i=0;i<4;i++){
-                int nrow = row+drow[i];
-                int ncol = col+dcol[i];
-                if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&visited[nrow][ncol]!=2&&grid[nrow][ncol]==1){
-                    visited[nrow][ncol]=2;
-                    q.push({{nrow,ncol},time+1});
+            for(int i=0;i<=3;i++){
+                int newRow = row+adjRow[i];
+                int newCol = col+adjCol[i];
+                if(newRow>=0&&newRow<n&&newCol>=0&&newCol<m&&visited[newRow][newCol]==0&&grid[newRow][newCol]==1){
+                    visited[newRow][newCol]=2;
+                    q.push({{newRow,newCol},time+1});
                 }
             }
         }
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(visited[i][j]!=2&&grid[i][j]==1)return -1;
+                if(visited[i][j]==0&&grid[i][j]==1)return -1;
             }
         }
         return t;
