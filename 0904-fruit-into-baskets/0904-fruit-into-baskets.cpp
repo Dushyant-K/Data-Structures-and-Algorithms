cbdc04f852362f0b3da09dae4d9cb5252cbb7f9d
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-08-04 08:17:00 +0530

    Time: 42 ms (51.81%), Space: 75.1 MB (31.99%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        //Approach-1(Sliding Window and two pointer)
+        // Approach-1(Sliding Window and two pointer)
         int n = fruits.size();
-        unordered_map<int,int> mpp;
+        unordered_map<int, int> mpp;
 
-        int left=0;
-        int right=0;
-        int maxi=0;
+        int left = 0;
+        int right = 0;
+        int maxi = 0;
 
-        while(right<n){
+        while (right < n) {
             mpp[fruits[right]]++;
 
-            while(mpp.size()>2&&left<=right){
+            if (mpp.size() > 2) {
                 mpp[fruits[left]]--;
-                if(mpp[fruits[left]]==0)mpp.erase(fruits[left]);
+                if (mpp[fruits[left]] == 0)
+                    mpp.erase(fruits[left]);
                 left++;
             }
 
-            maxi = max(maxi,right-left+1);
+            maxi = max(maxi, right - left + 1);
             right++;
         }
 
