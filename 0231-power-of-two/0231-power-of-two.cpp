7a0f0606303a507e6278d4d0bc4d0fff62f4b66c
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-08-09 10:12:16 +0530

    Time: 0 ms (100%), Space: 7.4 MB (100%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -3,8 +3,8 @@ public:
     bool isPowerOfTwo(int n) {
      int count=0;
      while(n>1){
-        if(n%2==1)count++;
-        n=n/2;
+        count+= n & 1;
+        n= n>>1;
      }
      if(n==1)count++;
      if(count==1)return true;
