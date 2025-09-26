3f1961f7a5e6ff1cc97d055e2fbcd70f9490365c
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2025-09-26 21:37:01 +0530

    Time: 331 ms (9.36%), Space: 179.8 MB (8.95%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        //Approach-1
-        // int n = nums.size();
-        // vector<int> ans;
-        // for(int i=0;i<=n-k;i++){
-        //     int maxi = nums[i];
-        //     for(int j=i;j<=(i+k-1);j++){
-        //         maxi = max(maxi,nums[j]);
-        //     }
-        //     ans.push_back(maxi);
-        // }
-        // return ans;
-
-        //Approach-2
-        deque<int> dq;
+        //Approach-1(Using Map)
         int n = nums.size();
+        map<int,int> mpp;
+
         vector<int> ans;
+        int left=0;
         for(int i=0;i<n;i++){
-            while(!(dq.empty())&&(dq.front()<(i-k+1))){
-                dq.pop_front();
+            mpp[nums[i]]++;
+            if(i>=k-1){
+                auto it = prev(mpp.end());
+                ans.push_back(it->first);
+
+                mpp[nums[left]]--;
+                if(mpp[nums[left]]==0)mpp.erase(nums[left]);
+                left++;
             }
-            while(!(dq.empty())&&(nums[dq.back()]<=nums[i])){
-                dq.pop_back();
-            }
-            dq.push_back(i);
-            if(i>=(k-1))ans.push_back(nums[dq.front()]);
         }
         return ans;
     }
