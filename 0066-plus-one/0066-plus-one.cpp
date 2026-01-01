0e11321b68f461a2cb7e2a3f25fba12ea5d10d79
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2026-01-01 08:54:24 +0530

    Time: 0 ms (100%), Space: 11.6 MB (50.17%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-         int n = digits.size();
+        //Approach-1
+        int n = digits.size();
 
-    // Traverse the digits array from the last digit to the first
-    for (int i = n - 1; i >= 0; --i) {
-        // If the digit is less than 9, just increment and return the result
-        if (digits[i] < 9) {
-            digits[i]++;
-            return digits;
+        int i=n-1;
+        while(i>=0){
+            if(digits[i]+1<10){
+                digits[i]=digits[i]+1;
+                break;
+            }else{
+                digits[i]=0;
+            }
+            i--;
         }
-        // If the digit is 9, set it to 0 and continue the loop
-        digits[i] = 0;
-    }
 
-    // If all the digits were 9, we need to add a new leading 1
-    digits.insert(digits.begin(), 1);
+        if(i==-1)digits.insert(digits.begin(),1);
 
-    return digits;
+        return digits;
     }
 };
