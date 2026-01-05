d51304e814a86d9ff26511ea5cb1b9008eae6e04
Dushyant Verma <83710811+Dushyant-K@users.noreply.github.com>
2026-01-05 15:12:57 +0530

    Time: 67 ms (77.29%), Space: 98.3 MB (44.71%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
@@ -3,50 +3,31 @@ public:
     int mostBooked(int n, vector<vector<int>>& meetings) {
         sort(meetings.begin(), meetings.end());
 
-        vector<int> count(n);
-        vector<long long> timer(n);
+        priority_queue<int, vector<int>, greater<int>> free;
+        priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<>> busy;
+        vector<int> cnt(n);
 
-        int itr = 0;
+        for (int i = 0; i < n; i++) free.push(i);
 
-        while (itr < meetings.size()) {
-            int start = meetings[itr][0];
-            int end = meetings[itr][1];
-            long long dur = end - start;
+        for (auto &m : meetings) {
+            long long s = m[0], d = m[1] - m[0];
 
-            int room = -1;
-            long long earliest = LLONG_MAX;
-            int earliestRoom = -1;
-
-            for (int i = 0; i < n; i++) {
-                if (timer[i] < earliest) {
-                    earliest = timer[i];
-                    earliestRoom = i;
-                }
-                if (timer[i] <= start) {
-                    room = i;
-                    break;
-                }
+            while (!busy.empty() && busy.top().first <= s) {
+                free.push(busy.top().second);
+                busy.pop();
             }
 
-            if (room != -1) {
-                timer[room] = end;
-                count[room]++;
+            if (!free.empty()) {
+                int r = free.top(); free.pop();
+                busy.push({m[1], r});
+                cnt[r]++;
             } else {
-                timer[earliestRoom] += dur;
-                count[earliestRoom]++;
-            }
-
-            itr++;
-        }
-
-        int max = 0, idx = 0;
-        for (int i = 0; i < n; i++) {
-            if (count[i] > max) {
-                max = count[i];
-                idx = i;
+                auto [t, r] = busy.top(); busy.pop();
+                busy.push({t + d, r});
+                cnt[r]++;
             }
         }
 
-        return idx;
+        return max_element(cnt.begin(), cnt.end()) - cnt.begin();
     }
 };
